8387db85e52dae83c48c3b5348c1e488e2dc5c3b
gribskov <mgribskov@gmail.com>
2017-11-14 10:18:50 -0500

    current version of test program, split.py

--- split.py
+++ split.py
@@ -4,14 +4,23 @@ split a sequence in fasta format into chunks
 from sequence.fasta import Fasta 
 import sys
 
-file = "../../data/card/nucleotide_fasta_protein_homolog_model.fasta"
+#file = "../../data/card/nucleotide_fasta_protein_homolog_model.fasta"
+file = "data/1seq.fa"
 fasta = Fasta()
 #fasta.open(sys.argv)
-fasta.open(file)
-fasta.read()
-print( "id:", fasta.id )
-print( "doc:", fasta.doc )
-print( "seq:", fasta.seq )
 
 print( "file:", fasta.filename )
+fasta.open(file)
+
+nseq = 0
+while fasta.next():
+	nseq += 1
+	print( "id:", fasta.id )
+	print( "doc:", fasta.doc )
+	print( "seq:", fasta.seq )
+	print( "length:", fasta.length() )
+	print( "write:", fasta.format() )
+
+	if nseq > 2: break
+
 
