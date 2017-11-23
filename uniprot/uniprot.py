24342abbd4749a086e760589f1f0e9b1b9cf191f
gribskov <mgribskov@gmail.com>
2017-11-23 08:32:27 -0500

    uniprot - getUniprotByIDList retrieves list of IDs

--- uniprot/uniprot.py
+++ uniprot/uniprot.py
@@ -4,19 +4,16 @@ uniprot access using REST api
 import urllib.request, urllib.parse, urllib.error
 import requests
 
-
 uniprot = 'http://www.uniprot.org/uniprot/'
-uniref  = 'http://www.uniprot.org/uniref/'
-batch   = 'http://www.uniprot.org/uploadlists/'
+uniref = 'http://www.uniprot.org/uniref/'
+batch = 'http://www.uniprot.org/uploadlists/'
 
 def getUnirefIDByMember(level, id):
     '''
     get the uniref90 group from the entry
-    sample query:
-    http://www.uniprot.org/uniref/?query=member:P99999+AND+identity:0.9
+    sample query: http://www.uniprot.org/uniref/?query=member:P99999+AND+identity:0.9
     usage
         text = getUnirefByMember( 'K4A607_SETIT', 90)
-
     '''
     if level == 90:
         level = 0.9
@@ -24,20 +21,21 @@ def getUnirefIDByMember(level, id):
         level = 0.5
 
     query = '{0}?query=member:{1}+AND+identity:{2}&format={3}'.format(uniref, id, level, 'list')
-    #print(query)
+    # print(query)
     fhand = urllib.request.urlopen(query)
     return fhand.readline().decode().strip()
+
 
 def getUniprotIDByUniref(uniref_id):
     '''
     get the list of entries in the uniref group, store in idlist
-    http://www.uniprot.org/uniref/UniRef90_A0MWC0&format=list
+    Generated query: http://www.uniprot.org/uniref/UniRef90_A0MWC0&format=list
     usage
         idlist = getUniprotIDByUniref(uniref_id)
     '''
     format = 'list'
     query = '{0}{1}&format={2}'.format(uniref, uniref_id, format)
-    #print(query)
+    # print(query)
     fhand = urllib.request.urlopen(query)
 
     idlist = []
@@ -46,6 +44,7 @@ def getUniprotIDByUniref(uniref_id):
 
     return idlist
 
+
 def getUniprotByID(id, format):
     '''
     retrieve the whole uniprot entry for each member
@@ -53,9 +52,8 @@ def getUniprotByID(id, format):
     usage
         text = getUniprotByID(id,'xml')
     '''
-
     query = '{0}{1}&format={2}'.format(uniprot, id, format)
-    #print(query)
+    # print(query)
     fhand = urllib.request.urlopen(query)
 
     text = ''
@@ -64,48 +62,26 @@ def getUniprotByID(id, format):
 
     return text
 
-def getUniprotByIDList( idlist ):
+
+def getUniProtByIDList(idlist):
+    """
+    Retrieves a list sequences from UniProt. See http://www.uniprot.org/faq/28 for building queries.
+    usage
+        text = getUniProtByIDList(idlist)
+    """
+    # convert idlist to string
     idstr = ''
     for id in idlist:
-        idstr += id+' '
-    print('idstr:', idstr)
+        idstr += id + ' '
+
     params = {
-        'from':'ACC+ID',
-        'to':'ACC',
-        'format':'xt',
-        'url2':idstr
+        'from': 'ACC+ID',
+        'to': 'ACC',
+        'format': 'txt',
+        'uploadQuery': idstr
     }
-
-    data = urllib.parse.urlencode(params)
-    data = data.encode('ascii')
-    headers = {'User-Agent':'Python'}
-    req = urllib.request.Request(batch, data,method='POST')
-    req.add_header('User-Agent','Python')
-    #contact = "gribskov@purdue.edu" # Please set your email address here to help us debug in case of problems.
-    with urllib.request.urlopen(req)as f:
-        print(f.read().decode('utf-8'))
-
-
-def _retrieveFromUniProt(idlist ):
-    """
-    Retrieves sequences from UniProt according to the parameters supplied. The
-    parameters should be in a dictionary of strings mapped to strings. Service
-    is the sub-url of the Uniprot database, e.g. "uniprot/" or "mapping/". See
-    conf.py and http://www.uniprot.org/faq/28 for building queries. Returns a
-    tuple of the raw data and the header.
-    """
-    idstr = ''
-    for id in idlist:
-        idstr += id+' '
-    print('idstr:', idstr)
-    params = {
-        'from':'ACC+ID',
-        'to':'ACC',
-        'format':'xt',
-        'url2':idstr
-    }
-    r = requests.post(batch,data=params)
-    print( r.text)
+    r = requests.post(batch, data=params)
+    return r.text
 
 
 if __name__ == '__main__':
@@ -116,9 +92,8 @@ if __name__ == '__main__':
     idlist = getUniprotIDByUniref(uniref_id)
     print('idlist:', idlist)
 
-    for id in idlist:
-        print(getUniprotByID(id,'txt'))
+    # for id in idlist:
+    #     print(getUniprotByID(id,'txt'))
 
-    getUniprotByIDList(idlist)
-    #resp = _retrieveFromUniProt(idlist)
-    #print(resp)
+    text = getUniProtByIDList(idlist)
+    print(text)
