740df3d95080093a936526217b3ecdbfb59f75d2
gribskov <mgribskov@gmail.com>
2017-11-24 16:30:41 -0500

    trinity - changed to use memory

---
+++ trinity.py
@@ -0,0 +1,80 @@
+'''
+
+'''
+from sequence.fasta import Fasta
+import re
+import sqlite3 as sql
+
+lenre  = re.compile('len=(\d+)')
+pathre = re.compile('.*path=\[([^\]]*)\]')
+
+def getLen(line):
+    '''
+    get the sequence length from the documentation
+    '''
+    return lenre.match(line).group(1)
+
+def getPath(line):
+    '''
+    return a list of path components from documentation
+    '''
+    #path = pathre.match(line).group(1)
+    # print('doc:', line)
+    # print('pre:',pathre)
+    # print('path:', pathre.match(line).group(1))
+    path = pathre.match(line).group(1)
+    plist = path.split(' ')
+    return plist
+
+def trinityID(id):
+    '''
+    Breakdown the trinity ID string to give the
+    Cluster,  component, gene and isoform
+     '''
+    cluster, component, gene, isoform = re.compile(r'>*TRINITY_([^_]+)_c(\d+)_g(\d+)_i(\d+)').match(id).groups()
+    return cluster, component, gene, isoform
+
+
+trinity = Fasta()
+
+file = r'C:\Users\gribs\Dropbox\rice\Trinity.fasta'
+trinity.fh = open(file, 'r')
+
+#dbh = sql.connect('trinity.db')
+dbh = sql.connect( ":memory:" )
+dbh.row_factory = sql.Row
+db = dbh.cursor()
+db.execute('DROP TABLE IF EXISTS transcript')
+db.execute( 'CREATE TABLE transcript (id TEXT, component INTEGER, gene INTEGER, isoform INTEGER)')
+
+nseq = 0
+while trinity.next():
+    nseq += 1
+
+    cluster, component, gene, isoform = trinityID(trinity.id)
+    #print('cluster:', cluster, 'component:', component, 'gene:', gene, 'isoform:', isoform)
+    db.execute( 'INSERT INTO transcript VALUES (?,?,?,?)',
+                (cluster, component, gene, isoform) )
+
+    # n = getLen(trinity.doc)
+    # print('m:', n,  '   ', trinity.doc)
+
+    nn=getPath(trinity.doc)
+    if len(nn)<=1: continue
+
+    print('doc:',trinity.doc)
+    print(trinity.seq)
+    for p in nn:
+        print('nn:', p)
+    #if nseq > 500: break
+    break
+
+db.execute( 'SELECT id, component, gene, isoform FROM transcript')
+n = 0
+for row in db:
+    n += 1
+    print(  row['id'])
+    for k in row.keys():
+        print( '    ', k, row[k])
+    #print( '    ', row['cluster'])
+
