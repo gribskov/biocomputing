aa2933cf2b6e85db256866cc22d5ea3cb6d4d62a
gribskov <mgribskov@gmail.com>
2017-12-02 10:29:06 -0500

    kollema.py - initial shell only, calls status

---
+++ kollema.py
@@ -0,0 +1,14 @@
+'''--------------------------------------------------------------------------------
+kollema
+Transcriptome annotation
+--------------------------------------------------------------------------------'''
+import sqlite3 as sql
+
+version = '0.0.1'
+dbfile = 'kollema.sql'
+
+print('Welcome to Kollema, v{0}'.format(version))
+
+# kdb = kollemadb(dbfile)
+
+exit(0)
