62d3a8de29b1a1b98bd017f49616f5ea6f20fb9d
Michael Gribskov <mgribsko@purdue.edu>
2017-12-03 16:56:14 -0500

    kollemadb.py - added basic get and set methods to fill tables. These will hae to be made more flexible, esp for non-string values.

--- kollemadb.py
+++ kollemadb.py
@@ -4,7 +4,7 @@ Database object for kollema transcriptome annotation tool
 --------------------------------------------------------------------------------'''
 import sys
 import re
-import sqlite3 as sql
+import sqlite3 as s3
 
 
 class Kollemadb(object):
@@ -16,8 +16,8 @@ class Kollemadb(object):
         :return: always true for now
         '''
         # isolation_level None is autocommit
-        dbh = sql.connect(dbfile, isolation_level=None)
-        dbh.row_factory = sql.Row
+        dbh = s3.connect(dbfile, isolation_level=None)
+        dbh.row_factory = s3.Row
         self.db = dbh.cursor()
 
         if new:
@@ -80,8 +80,8 @@ class Kollemadb(object):
         rtab = re.compile('\t$')
         rspace = re.compile('\s+')
         s = ''
-        #self.db.execute("SELECT * FROM sqlite_master")
-        self.db.execute("SELECT * FROM sqlite_master WHERE type='table'")
+        self.db.execute("SELECT * FROM sqlite_master")
+        #self.db.execute("SELECT * FROM sqlite_master WHERE type='table'")
         for row in kdb.db:
             for r in row:
                 if isinstance(r, str):
@@ -91,6 +91,57 @@ class Kollemadb(object):
             #s+= '\n'
             s = rtab.sub('\n',s)
         return s.strip()
+
+    def get(self, table, limits=''):
+        '''
+        retrieve contents of table as a dictionary
+        usage
+            project = kollemadb.get(project)
+            task = kollemadb.get(task)
+        '''
+        sql = '''
+            SELECT * FROM {0}
+            '''.format(table)
+        
+        try:
+            self.db.execute(sql)
+        except s3.Error as e:
+            print("Kollemadb::get - error:", e.args[0])
+            #sqlite3.OperationalError: no such table:
+
+        result = {}
+        row = self.db.fetchone()
+
+        for k in row.keys():
+            result[k] = row[k]
+
+        return result
+
+
+    def set(self, table, data):
+        '''
+        add a new row to an existing table
+        usage
+            kollemadb.set( table, data )
+        '''
+        columns = ''
+        values = ''
+        comma = ''
+        for k in data:
+            columns += '{0}{1} '.format(comma, k)
+            values += "{0}'{1}' ".format(comma, data[k])
+            comma = ','
+
+        sql = '''
+            INSERT INTO {0} 
+                ({1}) VALUES ({2})'''.format(table, columns.strip(), values.strip())
+
+        print('sql:', sql)
+        try:
+            self.db.execute(sql)
+        except s3.Error as e:
+            print("Kollemadb::set - error:", e.args[0])
+
 
 
 '''--------------------------------------------------------------------------------
@@ -102,3 +153,14 @@ if __name__ == '__main__':
     kdb = Kollemadb(new=True)
     print(kdb.showTables())
 
+    data = { 'name':'test',
+             'owner':'gribskov',
+             'status':'old'
+           }
+    kdb.set('project', data)
+    result = kdb.get('project')
+    for k in result:
+        print(k, result[k])
+
+    #result = kdb.get('task')
+    #result = b.get('transcript')
