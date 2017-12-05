6e3c5499722ec22aea591d0eb2ebda3e9571b484
Michael Gribskov <mgribsko@gmail.com>
2017-12-04 20:47:58 -0500

    kollema.py - updated to use Menu class

--- kollema.py
+++ kollema.py
@@ -4,41 +4,25 @@ Transcriptome annotation
 --------------------------------------------------------------------------------'''
 import sqlite3 as sql
 from kollemadb import Kollemadb
+from menu import Menu
 
-
-def menu(id, indent=2):
-    '''
-    print a menu string using input, and return the response
-    :param id: name of the menu
-    :param indent: number of spaces to indent, default=2
-    :return: selected response
-    '''
-    response = input('\n{0}{1}: '.format(' ' * indent, menudefs[id]))
-    return response.upper()[0]
-
-    # return response[0]
 
 def status():
     print('status activated')
     pass
 
-menudefs = {'top': 'S)tatus  T)asks  L)oad transcripts  Q)uit'}
-dispatch = {'S':status()}
 
 dbfile = 'kollema.sql'
 version = '0.0.1'
 print('Welcome to Kollema, v{0}'.format(version))
 
+menu = Menu()
+menu.add('top',{'S':'Status', 'T':'Tasks', 'L':'Load transcripts', 'Q':'Quit'})
+
 kdb = Kollemadb()
 while True:
-    select = menu('top', 2)
+    select = menu.ask('top', 2)
     if select == 'Q': break
-    print('  {0} selected'.format(select))
 
-    try:
-        dispatch[select]
-
-    except KeyError:
-         print('Unknown function selected ({0}'.format(select))
-
+print('\nThank you')
 exit(0)
