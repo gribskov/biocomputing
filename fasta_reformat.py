41db715122d61f4d3a91bde1e43d9116ed55a6ea
gribskov <gribskov@purdue.edu>
2018-01-09 11:51:06 -0500

    fasta_reformat.py - Tested and works on simple case.

---
+++ fasta_reformat.py
@@ -0,0 +1,30 @@
+"""---------------------------------------------------------------------------------------------------------------------
+reformat the output from the apc.pl de-circularization program.
+Input is two lines, idline and sequence
+Output is 100 letters/line
+
+usage
+    fasta_reformat.py *.fasta
+---------------------------------------------------------------------------------------------------------------------"""
+import glob
+import sys
+from sequence.fasta import Fasta
+
+linelen = 100
+
+# default target file name
+target = '*.fasta'
+if len(sys.argv) > 1:
+    target = sys.argv[1]
+print('  target file:', target)
+
+for fastafile in glob.glob(target):
+    # output file
+    outfile = fastafile + '.reformatted'
+    out = open(outfile, 'w')
+    print('  input file:', fastafile, '    output file:', outfile)
+
+    fasta = Fasta()
+    fasta.open(fastafile)
+    while fasta.next():
+        out.write(fasta.format(linelen=linelen))
