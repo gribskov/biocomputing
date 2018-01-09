0708b5017301cab763cbf5b47c4c2f5f2983c609
gribskov <mgribskov@gmail.com>
2018-01-09 09:12:38 -0500

    fasta - changed linelen to parameter. Added some documentation.

--- sequence/fasta.py
+++ sequence/fasta.py
@@ -1,15 +1,23 @@
-'''
+'''-------------------------------------------------------------------------------------------------
 Fasta sequence class.  Supports iteration over a multi-fasta file
+    filename
     id
     documentation
     sequence
-'''
+-------------------------------------------------------------------------------------------------'''
 
-
-# import re
 
 class Fasta:
+
     def __init__(self):
+        """-----------------------------------------------------------------------------------------
+        Fasta class constructor. Attributes
+            filename
+            id
+            doc
+            seq
+            buffer  (read ahead buffer, only internal)
+        -----------------------------------------------------------------------------------------"""
         self.filename = ''
         self.id = ''
         self.doc = ''
@@ -17,10 +25,10 @@ class Fasta:
         self.buffer = ''
 
     def open(self, filename):
-        '''
+        '''-----------------------------------------------------------------------------------------
         open a file for reading
-        '''
-        #print('opening:', filename)
+        -----------------------------------------------------------------------------------------'''
+        # print('opening:', filename)
         try:
             self.fh = open(filename, 'r')
         except:
@@ -29,18 +37,18 @@ class Fasta:
         self.filename = filename
 
     def next(self):
-        '''
+        '''-----------------------------------------------------------------------------------------
         return the next entry from an open file into the object
-        '''
+        -----------------------------------------------------------------------------------------'''
         self.read();
         return self.read()
 
     def read(self):
-        '''
+        '''-----------------------------------------------------------------------------------------
         read one sequence from the file, leave the following line in buffer
         usage:
         fasta.read()
-        '''
+        -----------------------------------------------------------------------------------------'''
 
         self.id = ''
         self.doc = ''
@@ -59,13 +67,13 @@ class Fasta:
         return False
 
     def getID(self):
-        '''
+        '''-----------------------------------------------------------------------------------------
         intended to be used internally for sequence reading
         check the buffer, and if not empty read the ID and documentation
         sore in id and doc attributes
         id will be stripped of >
         documentation will be and empty string if there is nothing following the ID
-        '''
+        -----------------------------------------------------------------------------------------'''
         # if buffer is empty read a line
         if self.buffer:
             line = self.buffer
@@ -86,41 +94,38 @@ class Fasta:
         self.doc = doc
 
     def length(self):
-        '''
+        '''-----------------------------------------------------------------------------------------
         return the length of the  current sequence
         return 0 if there is none
         usage
             seqlen = fasta.length()
-        '''
+        -----------------------------------------------------------------------------------------'''
         return len(self.seq)
 
-    def format(self):
-        '''
+    def format(self, linelen=50):
+        '''-----------------------------------------------------------------------------------------
         return a formatted string with the current sequence
         usage
             seq = fasta.format()
-        '''
-        # hardwired length for sequence lines
-        # TODO change to argument
-        LINELEN = 50
+        -----------------------------------------------------------------------------------------'''
 
         string = '>{0} {1}\n'.format(self.id, self.doc)
         pos = 0
         while pos < len(self.seq):
-            string += '{0}\n'.format(self.seq[pos:pos + LINELEN])
-            pos += LINELEN
+            string += '{0}\n'.format(self.seq[pos:pos + linelen])
+            pos += linelen
 
         return string
 
     def trimDocByRegex(self, target):
-        '''
+        '''-----------------------------------------------------------------------------------------
         Shorten documentation by substituting the target regex with nothing
         target must be a compiled regex
         The new documentation string is returned
         usage
             trim = re.compile( 'len=\d+ ' )
             doc = fasta.trimDocAfterMatch( trim )
-        '''
+        -----------------------------------------------------------------------------------------'''
         self.doc = target.sub('', self.doc)
 
         return self.doc
