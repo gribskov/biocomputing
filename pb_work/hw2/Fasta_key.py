628712c92e00d82182e7e8f70f16c349b7cc3a71
gribskov <mgribskov@gmail.com>
2018-02-13 08:52:27 -0500

    update with documentation. changed buffer to _buffer.

--- pb_work/hw2/Fasta_key.py
+++ pb_work/hw2/Fasta_key.py
@@ -1,9 +1,12 @@
 """=================================================================================================
-Fasta sequence class.  Supports iteration over a multi-fasta file
-    filename
-    id
-    documentation
-    sequence
+Fasta sequence class.  Supports iteration over a multi-fasta file.  Object have the following
+externally accessible attributes:
+
+    filename    input filename
+    fh          input filehandle
+    id          ID string (without>)
+    doc         documentation string
+    seq         sequence (one letter code, sequence characters only)
 
     Synopsis
     from fasta import Fasta
@@ -14,6 +17,7 @@ Fasta sequence class.  Supports iteration over a multi-fasta file
         fasta.next():
         print(fasta.format(linelen=60))
 
+TODO: Don't forget to put your name and the date here
 ================================================================================================="""
 
 
@@ -23,17 +27,18 @@ class Fasta:
         """-----------------------------------------------------------------------------------------
         Fasta class constructor. Attributes
             filename
+            fh
             id
             doc
             seq
-            buffer  (read ahead buffer, only internal)
+            _buffer  (read ahead buffer, internal use only)
         -----------------------------------------------------------------------------------------"""
         self.filename = ''
         self.fh = None
         self.id = ''
         self.doc = ''
         self.seq = ''
-        self.buffer = ''
+        self._buffer = ''
 
         return
 
@@ -47,7 +52,7 @@ class Fasta:
         try:
             self.fh = open(filename, 'r')
         except FileNotFoundError:
-            print('Fasta::open - file open error')
+            print('Fasta::open - {} not found'.format(filename))
             status = False
 
         self.filename = filename
@@ -56,7 +61,7 @@ class Fasta:
 
     def next(self):
         """-----------------------------------------------------------------------------------------
-        read one sequence from the file, leave the following line in buffer
+        read one sequence from the file, leave the following line in _buffer
         usage:
             fasta.read()
 
@@ -76,12 +81,12 @@ class Fasta:
 
             line = line.rstrip('\n')
             if line[0] == '>':
-                self.buffer = line
+                self._buffer = line
                 break
 
             else:
-                line = line.replace('N','')
-                line = line.replace('*','')
+                line = line.replace('N', '')
+                line = line.replace('*', '')
                 self.seq += line
 
         if len(self.id) > 0 or len(self.doc) > 0 or len(self.seq) > 0:
@@ -93,18 +98,18 @@ class Fasta:
     def getID(self):
         """-----------------------------------------------------------------------------------------
         intended to be used internally for sequence reading
-        check the buffer, and if not empty read the ID and documentation
+        check the _buffer, and if not empty read the ID and documentation
         sore in id and doc attributes
         id will be stripped of >
         documentation will be and empty string if there is nothing following the ID
 
         :return: None
         -----------------------------------------------------------------------------------------"""
-        # if buffer is empty read a line
+        # if _buffer is empty read a line
         line = ''
-        if self.buffer:
-            line = self.buffer
-            self.buffer = ''
+        if self._buffer:
+            line = self._buffer
+            self._buffer = ''
         else:
             for line in self.fh:
                 if line.isspace():
@@ -132,6 +137,7 @@ class Fasta:
             seq = fasta.format()
 
         :param linelen: number of sequence characters per line
+        :return: string containing formatted sequence
         -----------------------------------------------------------------------------------------"""
         string = '>{0} {1}\n'.format(self.id, self.doc)
         pos = 0
@@ -147,14 +153,27 @@ class Fasta:
 # ==================================================================================================
 if __name__ == '__main__':
 
-    fasta = Fasta()
+    outfile = 'hw2_key.out'
+    try:
+        out = open(outfile, 'w')
+    except:
+        print('unable to open {}'.format(outfile))
+        exit(1)
 
+    fasta = Fasta()
     fasta.open('short_broke.fa')
+
+    n = 0
     while True:
+        n += 1
         if fasta.next():
-            print('sequence {}:{}\nlength={}'.format(fasta.id, fasta.doc, len(fasta.seq)))
-            print('{}'.format(fasta.format(linelen=50)))
+            print('sequence {}:{} length={}'.format(n, fasta.id, len(fasta.seq)))
+            out.write('{}'.format(fasta.format(linelen=50)))
         else:
             break
 
+        if n >= 10:
+            break
+
+    out.close()
     exit(0)
