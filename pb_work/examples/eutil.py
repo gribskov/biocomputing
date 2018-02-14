176f78b3fea1c099197f9aa406d4bd8159d25836
Michael Gribskov <mgribsko@gmail.com>
2018-02-13 19:30:53 -0500

    examples for slides

---
+++ pb_work/examples/eutil.py
@@ -0,0 +1,24 @@
+"""=================================================================================================
+eutil.py
+retrieve information from NCBI  suing eutil REST interface
+================================================================================================="""
+import requests
+
+# https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=asthma&usehistory=y
+
+ncbi = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+esearch = 'esearch.fcgi?'
+
+query = 'db=protein&term=asthma&usehistory=y'
+response = requests.get(ncbi + esearch + query)
+print(response.content)
+
+print('\n', response.text)
+
+params = {'db': 'protein',
+          'term': 'asthma',
+          'usehistory': 'y'}
+
+esearch = 'esearch.fcgi'
+response = requests.post(ncbi + esearch, params)
+print('\n', response.text)
