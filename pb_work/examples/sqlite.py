701c9a6007d13aa6445434bc0670b4aa63b833f9
Michael Gribskov <mgribsko@gmail.com>
2018-03-04 14:21:14 -0500

    sqlite.py - converted to object

--- pb_work/examples/sqlite.py
+++ pb_work/examples/sqlite.py
@@ -6,64 +6,74 @@ Michael Gribskov     March 04  2018
 import sqlite3 as sq3
 
 
-def setupDBTables(db):
+class Genome():
     """---------------------------------------------------------------------------------------------
-    Creates all tables for the database
-    :param db: database cursos from SQLite3
-    :return: None
+    Persistent genome class, based on SQLite3
     ---------------------------------------------------------------------------------------------"""
-    sql = '''
-         CREATE TABLE IF NOT EXISTS chromosome
-         (   chr_id TEXT PRIMARY KEY,
-             length INTEGER
-         )   
 
-         '''
-    db.execute(sql)
+    def __init__(self, dbfile='genome.db', new=False):
+        """-----------------------------------------------------------------------------------------
+        creates the database connection and initializes tables
+        -----------------------------------------------------------------------------------------"""
+        self.dbh = sq3.connect(dbfile)
+        self.db = self.dbh.cursor()
 
-    sql = '''
-         CREATE TABLE IF NOT EXISTS gene
-         (   gene_id TEXT PRIMARY KEY,
-             chr_id TEXT,
-             begin INTEGER,
-             end INTEGER,
-             strand TEXT
-         )   
+        self.setupDBTables(new)
 
-         '''
-    db.execute(sql)
+        # end of __init__
 
-    sql = '''
-         CREATE TABLE IF NOT EXISTS exon
-         (   exon_id TEXT PRIMARY KEY,
-             gene_id TEXT,
-             begin INTEGER,
-             end INTEGER,
-             strand TEXT,
-             type
-         )   
+    def setupDBTables(self, new):
+        """-----------------------------------------------------------------------------------------
+        Creates all tables for the database
+        :param db: database cursos from SQLite3
+        :return: None
+        -----------------------------------------------------------------------------------------"""
+        if new:
+            self.db.execute('DROP TABLE IF EXISTS chromosome')
+            self.db.execute('DROP TABLE IF EXISTS gene')
+            self.db.execute('DROP TABLE IF EXISTS exon')
 
-         '''
-    db.execute(sql)
+        sql = '''
+             CREATE TABLE IF NOT EXISTS chromosome
+             (   chr_id TEXT PRIMARY KEY,
+                 length INTEGER
+             )   
+    
+             '''
+        self.db.execute(sql)
 
-    sql = '''
-         INSERT INTO chromosome 
-             VALUES (1, 30427671 )
-         '''
-    db.execute(sql)
+        sql = '''
+             CREATE TABLE IF NOT EXISTS gene
+             (   gene_id TEXT PRIMARY KEY,
+                 chr_id TEXT,
+                 begin INTEGER,
+                 end INTEGER,
+                 strand TEXT
+             )   
+    
+             '''
+        self.db.execute(sql)
 
-    return None
+        sql = '''
+             CREATE TABLE IF NOT EXISTS exon
+             (   exon_id TEXT PRIMARY KEY,
+                 gene_id TEXT,
+                 begin INTEGER,
+                 end INTEGER,
+                 strand TEXT,
+                 type
+             )   
+    
+             '''
+        self.db.execute(sql)
+
+        return None
 
 
 # --------------------------------------------------------------------------------------------------
-# main
+# main/testing
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-
-    dbh = sq3.connect('mydbfile.db')
-    db = dbh.cursor()
-    setupDBTables(db)
-
-    dbh.commit()
+    genome = Genome('mydbfile.db', new=True)
 
     exit(0)
