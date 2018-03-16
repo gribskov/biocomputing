9f45341a130dcb63d84e4d851a262d68092e188b
gribskov <mgribskov@gmail.com>
2018-03-16 14:46:13 -0400

    score.py - initial version, default DNA alphabet.

--- sequence/score.py
+++ sequence/score.py
@@ -1,18 +1,19 @@
-class score():
+class Score:
     """=============================================================================================
     sequence scoring table object
     ============================================================================================="""
     import sys
+
     def __init__(self, alphabet='ACGT'):
         """-----------------------------------------------------------------------------------------
         score constructor
         -----------------------------------------------------------------------------------------"""
-        self.alphabet = ''
+        self.alphabet = alphabet
         self.a2i = {}
         self.i2a = []
         self.table = []
 
-        alphabetIndex()
+        self.alphabetIndex()
 
     def alphabetIndex(self):
         """-----------------------------------------------------------------------------------------
@@ -20,12 +21,28 @@ class score():
         :return: alphabet size
         -----------------------------------------------------------------------------------------"""
         i = 0
-        for char in alphabet:
+        for char in self.alphabet:
             if char in self.a2i:
                 # duplicate character
+                sys.stderr.write(
+                    'Score:alphabetIndex - duplicate alphabet character in {}\n'.format(alphabet))
 
             self.a2i[char] = i
             self.i2a.append(char)
             i += 1
 
         return i
+
+# ==============================================================================================
+# Testing
+# ==============================================================================================
+if __name__ == '__main__':
+
+    score = Score()
+
+    print('Default alphabet and indices')
+    print('    alphabet:', score.alphabet)
+    print('    a2i:', score.a2i)
+    print('    i2a:', score.i2a)
+
+    exit(0)
