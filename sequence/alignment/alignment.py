23c3697e0717875905ee52eda7b37eaf577eff8c
gribskov <mgribskov@gmail.com>
2018-03-16 22:50:38 -0400

    alignment - fixed import, must be outside class.

--- sequence/alignment/alignment.py
+++ sequence/alignment/alignment.py
@@ -1,10 +1,12 @@
+from sequence.fasta import Fasta
+from sequence.score import Score
+
+
 class Alignment:
     """=============================================================================================
     Sequence alignment class
 
     ============================================================================================="""
-    from sequence.fasta import Fasta
-    from sequence.score import Score
 
     def __init__(self):
         self.s1 = Fasta()
@@ -18,7 +20,8 @@ class Alignment:
 # main/test
 # ==================================================================================================
 if __name__ == '__main__':
-
     align = Alignment()
+    align.s1.seq = 'aaa'
+    print(align.s1.format())
 
     exit(0)
