946db60bb7dc0d3dae4f1f69de4fb254da245f44
Michael Gribskov <mgribsko@gmail.com>
2018-03-18 13:09:58 -0400

    pipeline_manual.py - print for stdout and stderr

--- pb_work/examples/pipeline_manual.py
+++ pb_work/examples/pipeline_manual.py
@@ -5,17 +5,17 @@ Michael Gribskov     March 18  2018
 ================================================================================================="""
 import subprocess as sub
 
-
 # --------------------------------------------------------------------------------------------------
 #
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-
     project = 'pipe_m1'
     input = ['data/s1_R1.fastq', 's1_R2.fastq']
 
     # create the project directory
-    mkproject = sub.run(['mkdir', project], stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+    mkproject = sub.run(['mkdir', project], stdout=sub.PIPE, stderr=sub.PIPE,
+                        universal_newlines=True, shell=True)
+    prnt('commnd:', mkproject.args())
     print('out|err:', mkproject.stdout, '|', mkproject.stderr)
 
     exit(0)
