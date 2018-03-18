590894d5940db72bcdb32ebccec4ad5d363c0263
Michael Gribskov <mgribsko@purdue.edu>
2018-03-18 15:04:09 -0400

    pipeline_manual.py - simple example with mkdir and head

--- pb_work/examples/pipeline_manual.py
+++ pb_work/examples/pipeline_manual.py
@@ -10,12 +10,30 @@ import subprocess as sub
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     project = 'pipe_m1'
-    input = ['data/s1_R1.fastq', 's1_R2.fastq']
+    input = ['s1_R1.fastq', 's1_R2.fastq']
 
     # create the project directory
-    mkproject = sub.run(['mkdir', project], stdout=sub.PIPE, stderr=sub.PIPE,
-                        universal_newlines=True, shell=True)
-    prnt('commnd:', mkproject.args())
-    print('out|err:', mkproject.stdout, '|', mkproject.stderr)
+    dir = project
+    mkproject = sub.run(['mkdir {}'.format(project)], stdout=sub.PIPE, stderr=sub.PIPE,
+                       universal_newlines=True, shell=True)
+
+
+    # create the subdirectory for the head step
+    dir += '/head'
+    mkhead = sub.run(['mkdir {}'.format(dir)], stdout=sub.PIPE, stderr=sub.PIPE,
+                       universal_newlines=True, shell=True)
+
+    # run the head command
+    for filein in input:
+        headout = dir + '/' + filein.replace('fastq', 'fastq.head')
+        print('head output:', headout)
+        head = open( headout, 'w')
+
+        command = 'head -100 {}'.format(filein)
+        runhead = sub.call([command ], stdout=head, stderr=sub.PIPE,
+                       universal_newlines=True, shell=True)
+
+        head.close()
+
 
     exit(0)
