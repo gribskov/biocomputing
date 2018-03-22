cc50f6829775156123244af452297a94183789db
Michael Gribskov <mgribsko@purdue.edu>
2018-03-21 21:15:43 -0400

    oligomm.py - converted all word reading/writing to object. Replaced Fasta class with custom sequence reader.

--- oligo/oligomm.py
+++ oligo/oligomm.py
@@ -3,11 +3,9 @@ get a count of kmers in a sequence.  The real goal is to identify the infrequent
 to probabilistically generate longer kmers unlikely to occur in the sequence.
 ================================================================================================="""
 import sys
-import pprint
-from math import log2 as log
+from math import log10 as log
 import random
 import itertools
-from sequence.fasta import Fasta
 
 
 class Kmer:
@@ -24,6 +22,10 @@ class Kmer:
         self.k = k
         self.alphabet = 'ACGT'
         self.kmer = {}
+        self.total = 0
+        self.p = {}
+        self.pmin = 1.0
+        self.pmax = 0.0
 
     def setupWords(self, prior=1):
         """-----------------------------------------------------------------------------------------
@@ -33,54 +35,120 @@ class Kmer:
         -----------------------------------------------------------------------------------------"""
         n = 0
         for word in itertools.product(self.alphabet, repeat=self.k):
-            self.kmer[''.join(word)] = prior
+            kstr = ''.join(word)
+            self.kmer[kstr] = prior
+            self.p[kstr] = 0.0
             n += 1
 
         return n
 
+    def fromFasta(self, file):
+        """-----------------------------------------------------------------------------------------
+        count kmers in a Fasta file.  Some words may be excluded because they contain non-alphabet
+        characters.
 
-# --------------------------------------------------------------------------------------------------
+        :param file:
+        :return: number of kmers counted
+        -----------------------------------------------------------------------------------------"""
+        fasta = None
+        try:
+            fasta = open(file, 'r')
+        except OSError:
+            sys.stderr.write('kmer.fromFasta - unable to open output file ({})'.format(file))
+
+        # count words in sequence
+        self.total = 0
+        nseq = 0
+        seq = ''  # for overlap
+        for line in fasta:
+            line = line.rstrip()
+            if line.startswith('>'):
+                nseq += 1
+                print('\nn', nseq)
+                seq = ''  # no overlap between sequences
+            else:
+                line = seq + line
+                for i in range(len(line) - KMER + 1):
+                    try:
+                        self.kmer[line[i:i + KMER]] += 1
+                        if not self.total % 10000000:
+                            print()
+                            print('{:13d} '.format(self.total), end='')
+                        if not self.total % 100000:
+                            print('.', end='')
+                        self.total += 1
+
+                    except KeyError:
+                        # kmers with non ACGT letters
+                        # sys.stderr.write('unknown kmer: {}\n'.format(line[i:i + KMER]))
+                        pass
+                # TODO: remove for production
+                if self.total > 10000000:
+                    break
+
+                sys.stdout.flush()
+                seq = line[-KMER:-1]    # this is the KMER-1 letters that overlap the next line
+
+        return self.total
+        # end of fromFasta
+
+    def updateProb(self):
+        """-----------------------------------------------------------------------------------------
+        Update the probabilities from the current counts.  Assumes the count in self.total is
+        correct.
+
+        :return: pmin, pmax
+        -----------------------------------------------------------------------------------------"""
+        self.pmin = 1.0
+        self.pmax = 0.0
+
+        for word in self.kmer:
+            self.p[word] = self.kmer[word] / self.total
+            self.pmin = min(self.pmin, self.p[word])
+            self.pmax = max(self.pmax, self.p[word])
+
+        return self.pmin, self.pmax
+
+    def tableWrite(self, file):
+        """-----------------------------------------------------------------------------------------
+        write the kmer table to a file
+        :param file: file name
+        :return: kmers written
+        -----------------------------------------------------------------------------------------"""
+        kmerout = None
+        try:
+            kmerout = open(file, 'w')
+        except OSError:
+            sys.stderr.write('kmer.tablewrite - unable to open output file ({})'.format(file))
+
+        total = 0
+        for word in self.kmer:
+            kmerout.write('{}\t{}\t{:.4g}\n'.format(word, self.kmer[word], self.p[word]))
+            total += self.kmer[word]
+
+        return total
+
+
+# ==================================================================================================
+# main
+# ==================================================================================================
 if __name__ == '__main__':
-    sys.stdout.flush()
 
-    fasta = Fasta()
-    fasta.open(sys.argv[1])
-
-    KMER = 6
+    KMER = 8
     kmer = Kmer(KMER)
     nwords = kmer.setupWords()
     print('{} words initialized'.format(nwords))
 
-    # count words in sequence
-    total = 0
-    nseq = 0
-    while fasta.next():
-        nseq += 1
-        if not nseq % 1000:
-            print('.', end='', flush=True)
-        if not nseq % 20000:
-            print()
+    nwords = kmer.fromFasta(sys.argv[1])
+    print('\ntotal {}mer words read from {}: {}'.format(KMER, sys.argv[1], nwords))
 
-        for i in range(len(fasta.seq) - KMER + 1):
-            kmer.kmer[fasta.seq[i:i + KMER]] += 1
-            total += 1
-        if nseq > 100:
-            break
+    # convert word counts to probabilities and write out
+    pmin, pmax = kmer.updateProb()
+    print('    pmin pmax: {:10.3g}\t{:10.3g}'.format(pmin, pmax))
+    print('log pmin pmax: {:10.3g}\t{:10.3g}'.format(log(pmin), log(pmax)))
 
-    print('\ntotal {}mer words: {}'.format(KMER, total))
-    pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(kmer.kmer)
-
-    # convert word counts to probabilities
-    pmin = 1.0
-    pmax = 0.0
-    for word in kmer.kmer:
-        kmer.kmer[word] /= total
-        pmin = min(pmin, kmer.kmer[word])
-        pmax = max(pmax, kmer.kmer[word])
-
-    print('p: {:10.3g}{:10.3g}'.format(pmin, pmax))
-    print('p: {:10.3g}{:10.3g}'.format(log(pmin), log(pmax)))
+    nwritten = kmer.tableWrite(sys.argv[2])
+    print('\n{} kmers written to {}'.format(nwritten, sys.argv[2]))
 
     # weight counts - ad hoc weighting function
     # w = bias**(logP - logPmax)
@@ -94,7 +162,7 @@ if __name__ == '__main__':
     # for sorted list
     #  for word in sorted(kmer.kmer, key=lambda k: kmer.kmer[k]):
     for word in kmer.kmer:
-        kmer.kmer[word] = 4**(cutoff - log(kmer.kmer[word]))
+        kmer.kmer[word] = 4 ** (cutoff - log(kmer.kmer[word]))
         wsum += kmer.kmer[word]
         wmin = min(wmin, kmer.kmer[word])
         wmax = max(wmax, kmer.kmer[word])
@@ -108,7 +176,7 @@ if __name__ == '__main__':
     omer = {}
     for word in itertools.product(kmer.alphabet, repeat=overlap):
         oword = ''.join(word)
-        omer[oword] = {'list': [], 't':[], 'w': 0.0}
+        omer[oword] = {'list': [], 't': [], 'w': 0.0}
 
     # calculate the transitions between overlapping words: 'list' is the list of overlapping full
     # words, 't' has the threshold value for each word (cumulative weigth), and 'w' has the total
@@ -121,7 +189,7 @@ if __name__ == '__main__':
 
     # construct an oligo
 
-    #select a weighted start point
+    # select a weighted start point
     r = random.random() * wsum
     found = ''
     for word in kmer.kmer:
@@ -143,5 +211,3 @@ if __name__ == '__main__':
             if r < omer[lap]['t'][i]:
                 break
         oligo += found[-overlap:]
-
-
