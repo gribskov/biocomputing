d039fa113546da3a56b658e4e12cf9c9e352a3f9
gribskov <gribskov@purdue.edu>
2018-03-27 14:05:32 -0400

    pbwork/examples/tree - childAdd() makes constructing new nodes simpler.

--- pb_work/examples/tree/tree.py
+++ pb_work/examples/tree/tree.py
@@ -39,28 +39,40 @@ class Tree():
 
         StopIteration
 
+    def childAdd(self, name=''):
+        """-----------------------------------------------------------------------------------------
+        Create a new child node AND add to children of current node.  Return the new node
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        new = Tree(name)
+        self.children.append(new)
+
+        return new
+
+
 # ==================================================================================================
 # testing
 # ==================================================================================================
 if __name__ == '__main__':
+
+    # test entry of information into tree node
+    print('\n-------------------------\ntree a with children b, c\n-------------------------')
+
     # create a tree
     tree = Tree('a')
 
-    # test entry of information into tree node
-    print('\n-------------------------\ntree a with children b, c\n------------------------')
-
-    print(str(tree))
-
     # add child nodes to a
-    b = Tree('b')
-    c = Tree('c')
-
-    tree.children.append(b)
-    tree.children.append(c)
+    b = tree.childAdd('b')
+    c = tree.childAdd('c')
     print('\n{}'.format(str(tree)))
+
+    # add a child to b
+    b.childAdd('d')
 
     # test dfs iterator
     print('\n-------------\ndfs iteration\n-------------')
+    print('expected order:a, b, d, c')
+
     for node in tree.dfs():
         print('\n{}'.format(str(node)))
 
