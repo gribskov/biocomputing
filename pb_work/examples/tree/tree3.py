03eb9748e4e2337d8cf20d48f43c2f8b995840d2
gribskov <gribskov@purdue.edu>
2018-03-27 11:54:47 -0400

    pbwork/examples/tree - Tree class examples

---
+++ pb_work/examples/tree/tree3.py
@@ -0,0 +1,52 @@
+class Tree():
+    """=============================================================================================
+    Class for multifurcating trees.
+
+    27 March 2018     Michael Gribskov
+    ============================================================================================="""
+
+    def __init__(self):
+        """-----------------------------------------------------------------------------------------
+        Tree constructor.
+        -----------------------------------------------------------------------------------------"""
+        self.name = ''
+        self.children = []
+
+    def __str__(self):
+        """-----------------------------------------------------------------------------------------
+        return a formatted string describing a tree node
+        :return: string
+        -----------------------------------------------------------------------------------------"""
+        treestr = 'node:{}\n    children: '.format(self.name)
+        if self.children:
+            for child in self.children:
+                treestr += str(child.name) + ' '
+
+        else:
+            treestr += 'None'
+
+        return treestr
+
+
+# ==================================================================================================
+# testing
+# ==================================================================================================
+if __name__ == '__main__':
+    # create a tree
+    tree = Tree()
+
+    # test entry of information into tree node
+    tree.name = 'a'
+    print(str(tree))
+
+    # add child nodes to a
+    b = Tree()
+    b.name = 'b'
+    c = Tree()
+    c.name = 'c'
+
+    tree.children.append(b)
+    tree.children.append(c)
+    print('\n{}'.format(str(tree)))
+
+    exit(0)
