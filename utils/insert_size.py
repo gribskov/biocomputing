71209a0424a6bffe2694a1588ac9f59c3c7cd918
Michael Gribskov <mgribsko@purdue.edu>
2018-04-01 08:28:37 -0400

    insert_size - opens and read sam file

---
+++ utils/insert_size.py
@@ -0,0 +1,33 @@
+"""=================================================================================================
+insert_size.py
+
+Calulate insert size based on a SAM file of mapped reads. To get only high quality mapped read pairs
+use the samtools command samtools view -q 20 -f 0x82 SRR5295840.bam > SRR5295840.mapped
+SAM format is (all one line, whitespace separated fields)
+
+SRR5295840.120  163     AT1G07250.1     101     44      1S150M  =       347     398     NCT...CAG
+#A<...FJF AS:i:300     XN:i:0   XM:i:0  XO:i:0  XG:i:0  NM:i:0  MD:Z:150        YS:i:300        YT:Z:CP
+
+the insert length is field 8, the last field before the sequence
+
+Michael Gribskov     1 April 2018
+================================================================================================="""
+import sys
+
+map = None
+try:
+    map = open(sys.argv[1], 'r')
+except:
+    print('unable to open input file ({}'.format(sys.argv[1]))
+    exit(1)
+
+nread = 0
+for line in map:
+    nread += 1
+    print(line)
+    if nread > 10:
+        break
+
+print('{} reads read from {}'.format(nread, sys.argv[1]))
+
+exit(0)
