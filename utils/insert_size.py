e1bd1b4867412302c0b7cd8987a12308b91cfc0b
Michael Gribskov <mgribsko@purdue.edu>
2018-04-01 10:45:50 -0400

    insert_size - print mean and sd on plot.  draws average line

--- utils/insert_size.py
+++ utils/insert_size.py
@@ -15,6 +15,7 @@ Michael Gribskov     1 April 2018
 import sys
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import statistics as stat
 
 map = None
 try:
@@ -28,21 +29,34 @@ lendata = []
 for line in map:
     nread += 1
     field = line.split()
-    print('{}\t{}'.format(field[0], field[8]))
+    # print('{}\t{}'.format(field[0], field[8]))
     insert = float(field[8])
     insert = max( insert, -insert)
     lendata.append(insert)
 
-    if nread > 1000:
+    if nread > 100000:
         break
 
+lenmean = stat.mean(lendata)
+lensd = stat.stdev(lendata)
+
 print('\n{} reads read from {}'.format(nread, sys.argv[1]))
+print('mean: {:.3f}\tstandard devation: {:.3f}'.format( lenmean, lensd))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
 n, bins, patches = plt.hist(lendata, bins=100, normed=1,
                             facecolor='blue', edgecolor='black', linewidth=0.25, alpha=0.75 )
 
 plt.xlabel('Length')
 plt.ylabel('Probability')
 plt.title('Library Insert Length')
+plt.grid(True, linestyle='-', linewidth=0.1)
+
+plt.text( 0.02, 0.9, 'mean: {:.1f}\nstandard deviation: {:.1f}'.format(lenmean, lensd), fontsize=10, transform=ax.transAxes)
+plt.axvline( lenmean, color='red', linewidth=1.5)
+
 plt.show()
 
 exit(0)
