7caf1916e1b1edc7608691e6360b10c91d794535
Michael Gribskov <mgribsko@purdue.edu>
2018-04-01 13:59:41 -0400

    read_dist - fixed problem with overrunning the end of the seq array.  new function extend_list().  Added filtering for uninterpretable SAM lines.

--- utils/read_dist.py
+++ utils/read_dist.py
@@ -20,33 +20,112 @@ NCT...CAG       SEQ     sequence
 
 the remaining columns are application specific
 AS:i:300     XN:i:0   XM:i:0  XO:i:0  XG:i:0  NM:i:0  MD:Z:150        YS:i:300        YT:Z:CP
+
+Michael Gribskov    1 April 2018
 ================================================================================================="""
 import sys
+
+
+def extend_list(arr, end, init=0):
+    """---------------------------------------------------------------------------------------------
+    extend the existing list arr by adding indices from the current end of the list to the specified
+    end pos (the new last index in list)
+
+    :param arr: list
+    :param end: last index to create
+    :param init: value to initialize elements with
+    :return: int, new list size
+    ---------------------------------------------------------------------------------------------"""
+    begin = len(arr)
+    arr += [init for k in range(begin, end + 1)]
+
+    return len(arr)
+
+
+def add_cigar(seq, pos, cigar):
+    """---------------------------------------------------------------------------------------------
+    increment the count of reads at the corresponding positions of sort given the start pos and
+    CIGAR string
+
+    :param seq: list corresponding to sequence bases
+    :param pos: beginning position of mapped read
+    :param cigar: alignment string
+    :return: integer, bases incremented (number of M)
+    ---------------------------------------------------------------------------------------------"""
+    istr = ''
+    m = 0
+    for char in cigar:
+        if char.isdigit():
+            istr += char
+            continue
+
+        i = int(istr)
+        istr = ''
+        if char == 'M':
+            # matching positions
+
+            #check to make sure seq list is big enough, if not add some more elements
+            if pos + i + 1 > len(seq):
+                extend_list(seq, pos + i + 10000)
+
+            for j in range(pos, pos + i - 1):
+                m += 1
+                seq[j] += 1
+
+        elif char not in 'SD':
+            # must be a character we don't care about, we'll just ignore these for now
+            # I gets dealt with here
+            continue
+
+        # only M, S, and D fall through to here
+        #  M, S, and D increment the position, S and D do nothing else
+        pos += i - 1
+
+        return m
+
+    # end of add_cigar
+
 
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-map = None
-try:
-    map = open(sys.argv[1], 'r')
-except:
-    print('unable to open input file ({}'.format(sys.argv[1]))
-    exit(1)
+    map = None
+    try:
+        map = open(sys.argv[1], 'r')
+    except:
+        print('unable to open input file ({}'.format(sys.argv[1]))
+        exit(1)
 
-nread = 0
-seq = []
+    nread = 0
+    seq = []
 
-# assume that the mapped read begins at POS
-# use the CIGAR string to increment counts in the seq array
+    # assume that the mapped read begins at POS
+    # use the CIGAR string to increment counts in the seq array
 
-for line in map:
-    nread += 1
-    field = line.split()
-    # print('{}\t{}'.format(field[0], field[8]))
-    pos = int(field[3])
-    cigar = field[5]
+    bases_mapped = 0
+    for line in map:
+        if line.startswith('@'):
+            # skip header lines
+            continue
 
-    if nread > 1000:
-        break
+        field = line.split()
+        # print('{}\t{}'.format(field[0], field[8]))
+        pos = int(field[3])
+        mapq = field[4]
+        cigar = field[5]
+
+        # filter some lines
+        if mapq==0 or cigar =='*':
+            continue
+
+        nread += 1
+        bases_mapped += add_cigar(seq, pos, cigar)
+
+        if nread > 100000:
+            break
+
+    print('{} bases mapped'.format(bases_mapped))
+
+    exit(0)
