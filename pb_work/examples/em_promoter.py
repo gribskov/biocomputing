b166cb8fc8cad0c5d4b096220041bdd6d39cee4d
Michael Gribskov <mgribsko@gmail.com>
2018-04-08 13:26:55 -0400

    em_promoter_ added init_random() and expectation()

--- pb_work/examples/em_promoter.py
+++ pb_work/examples/em_promoter.py
@@ -4,6 +4,7 @@ Example of EM for promoter identification
 Michael Gribskov     08 April 2018
 ================================================================================================="""
 import sys
+import random
 
 
 class pssm():
@@ -29,18 +30,64 @@ class pssm():
 
     def initRandom(self, p):
         """-----------------------------------------------------------------------------------------
-
+        Fill pssm with background composition +/- random value [-p, +p)
         :param p: fraction of random noise to add to composition
         :return: float min, float max
         -----------------------------------------------------------------------------------------"""
+        # conversion for random [0.0, 1.0) to [-p, +p)
+        # p = = (r-0.5)2p = 2pr - p
+        twop = p * 2
+        # initialize pssm and column sums
+        colsum = [0 for k in range(self.size)]
+        self.pssm = [[0 for k in range(self.size)] for a in range(len(self.alphabet))]
+
+        #  calculate column sums as we go
+        i = 0
+        colsum = [0 for k in range(self.size)]
+        for letter in self.alphabet:
+            # rows are sequence characters
+            for pos in range(self.size):
+                r = random.random() * twop - p
+                self.pssm[i][pos] = self.composition[letter] + r
+                colsum[pos] += self.composition[letter] + r
+            i += 1
+
+        # convert pssm values to probabilities by dividing by column sums
+        valmin = 1.0
+        valmax = 0.0
+        i = 0
+        for letter in self.alphabet:
+            # rows are sequence characters
+            for pos in range(self.size):
+                self.pssm[i][pos] /= colsum[pos]
+                valmin = min(self.pssm[i][pos], valmin)
+                valmax = max(self.pssm[i][pos], valmax)
+            i += 1
+
+        return valmin, valmax
 
     def expectation(self):
         """-----------------------------------------------------------------------------------------
         Expectation step
-        Calculate that each sequenc position is a site
-
+        Calculate that each sequence position is a site. scores for each sequence are not normalized
+        to sum to 1 across the sequences
+        :return: True
         -----------------------------------------------------------------------------------------"""
-        pass
+        seqnum = 0
+        self.expected = []
+        for s in self.sequence:
+            exp = []
+            self.expected.append(exp)
+            for pos in range(len(s) - self.size + 1):
+                i = self.alphabet.find(s[pos])
+                p = self.pssm[i][0]
+                for w in range(1,self.size):
+                    i = self.alphabet.find(s[pos+w])
+                    p *= self.pssm[i][w]
+                exp.append(p)
+            seqnum += 1
+
+        return True
 
     def ml(self):
         """-----------------------------------------------------------------------------------------
@@ -124,7 +171,7 @@ if __name__ == '__main__':
     model = pssm(6, seqfile=sys.argv[1])
 
     # create random pssm from random frequencies +/- random
-    p_rand = 0.1
+    p_rand = 0.05
     model.initRandom(p_rand)
 
     # EM - until convergence
