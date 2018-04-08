d622681a5b3dd638454ded03df65c234ebb4b492
Michael Gribskov <mgribsko@gmail.com>
2018-04-08 10:08:34 -0400

    em_promoter - outline

---
+++ pb_work/examples/em_promoter.py
@@ -0,0 +1,26 @@
+"""=================================================================================================
+Example of EM for promoter identification
+
+Michael Gribskov     08 April 2018
+================================================================================================="""
+
+# ==================================================================================================
+#
+# ==================================================================================================
+if __name__ == '__main__':
+
+    # read promoter sequences and composition
+
+    # create random pssm from random frequencies +/- random
+
+    # EM - until convergence
+
+        # calculate position probabilities for each sequence (expectation)
+
+        # calculate maximimumlikelihood pssm
+
+    # report probability distributions
+
+    # report pssm
+
+    exit(0)
