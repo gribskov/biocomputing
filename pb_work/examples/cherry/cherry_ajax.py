b0a707720317adc0fdb3873f0f06fd514a4dfd59
Michael Gribskov <mgribsko@gmail.com>
2018-04-15 13:36:04 -0400

    cherry_ajax - somewhat working

--- pb_work/examples/cherry/cherry_ajax.py
+++ pb_work/examples/cherry/cherry_ajax.py
@@ -4,6 +4,7 @@ cherryPy app with ajax/jquery
 Michael Gribskov     15 April 2018
 ================================================================================================="""
 import os
+import sqlite3 as sq3
 
 import cherrypy
 from cherrypy.lib.static import serve_file
@@ -21,9 +22,24 @@ class GffCherry:
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def getData(self):
+    def getData(self, dbfile='gff.db'):
+        dbh = sq3.connect(dbfile)
+        db = dbh.cursor()
+
+        sql = 'SELECT DISTINCT feature FROM gff;'
+        db.row_factory = sq3.Row
+        db.execute(sql)
+        featurelist = []
+        features = ''
+        for row in db:
+            for key in row.keys():
+                if row['feature']:
+                    featurelist.append(row['feature'])
+
+        features = ','.join(featurelist)
+
         return {
-            'foo': 'bar',
+            'foo': features,
             'baz': 'another one'
         }
 
@@ -38,11 +54,14 @@ if __name__ == '__main__':
     config = {
         'global': {
             'server.socket_host': '127.0.0.1',
-            'server.socket_port': 8080,
+            'server.socket_port': 8081,
             'server.thread_pool': 8
         }
     }
 
     cherrypy.quickstart(GffCherry(), '/', config)
+
+
+    cherrypy.engine.exit()
 
     exit(0)
