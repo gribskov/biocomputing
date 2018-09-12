cf695b2253b292fd38b2d869cc5534dfd0545fb8
gribskov <mgribskov@gmail.com>
2018-09-12 07:37:07 -0400

    alignment - fixed errors in index()

--- sequence/alignment/alignment.py
+++ sequence/alignment/alignment.py
@@ -16,25 +16,25 @@ class Alignment:
         self.score = Score()
 
 
-def index(self, i1=True, i2=True):
-    """---------------------------------------------------------------------------------------------
-    cretae an integer array version of the sequences.  this makes it easier to lookup scores.  i1
-    and i2 indicate whether sequence 1 and sequence 2 should be indexed.
-    
-    :param self:
-    :return:
-    ---------------------------------------------------------------------------------------------"""
-    score = self.score
-    if i1:
-        self.i1 = []
-        for letter in self.s1:
-            self.i1.append(score.a2i(letter))
-    if i2:
-        self.i2 = []
-        for letter in self.s2:
-            self.i2.append(score.a2i(letter))
+    def index(self, i1=True, i2=True):
+        """-----------------------------------------------------------------------------------------
+        cretae an integer array version of the sequences.  this makes it easier to lookup scores.
+        i1  and i2 indicate whether sequence 1 and sequence 2 should be indexed.
 
-    return len(self.i1), len(self.i2)
+        :param self:
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        score = self.score
+        if i1:
+            self.i1 = []
+            for letter in self.s1.seq:
+                self.i1.append(score.a2i[letter])
+        if i2:
+            self.i2 = []
+            for letter in self.s2.seq:
+                self.i2.append(score.a2i[letter])
+
+        return len(self.i1), len(self.i2)
 
 
 # ==================================================================================================
@@ -42,7 +42,12 @@ def index(self, i1=True, i2=True):
 # ==================================================================================================
 if __name__ == '__main__':
     align = Alignment()
-    align.s1.seq = 'aaa'
+    align.s1.seq = 'AGGC'
     print(align.s1.format())
+
+    align.s2.seq = "AGCGT"
+    print(align.s2.format())
+
+    align.index()
 
     exit(0)
