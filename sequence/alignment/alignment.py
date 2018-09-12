27de6603207823e5df2c63970fc6b0530f555a69
gribskov <mgribskov@gmail.com>
2018-09-12 07:30:29 -0400

    alignment - added index()

--- sequence/alignment/alignment.py
+++ sequence/alignment/alignment.py
@@ -16,6 +16,27 @@ class Alignment:
         self.score = Score()
 
 
+def index(self, i1=True, i2=True):
+    """---------------------------------------------------------------------------------------------
+    cretae an integer array version of the sequences.  this makes it easier to lookup scores.  i1
+    and i2 indicate whether sequence 1 and sequence 2 should be indexed.
+    
+    :param self:
+    :return:
+    ---------------------------------------------------------------------------------------------"""
+    score = self.score
+    if i1:
+        self.i1 = []
+        for letter in self.s1:
+            self.i1.append(score.a2i(letter))
+    if i2:
+        self.i2 = []
+        for letter in self.s2:
+            self.i2.append(score.a2i(letter))
+
+    return len(self.i1), len(self.i2)
+
+
 # ==================================================================================================
 # main/test
 # ==================================================================================================
