ea3feabe1545cabc76906f2fedce3c526f41890e
gribskov <mgribskov@gmail.com>
2018-09-14 08:36:19 -0400

    alignment - nearly working prototype

--- sequence/alignment/alignment.py
+++ sequence/alignment/alignment.py
@@ -14,6 +14,8 @@ class Alignment:
         self.i1 = []
         self.i2 = []
         self.score = Score()
+        self.smat = None
+        self.pmat = None
         self.gi = 0  # length independent gap penalty
         self.gd = 0  # length dependent gap penalty
 
@@ -37,6 +39,37 @@ class Alignment:
 
         return len(self.i1), len(self.i2)
 
+    def matString(align, width=2):
+        """-----------------------------------------------------------------------------------------
+        return a string with the formatted score and path matrices
+        :param width: int, field width of columns
+        :return: string
+        -----------------------------------------------------------------------------------------"""
+        s = 'score\n\n    '
+
+        for j in range(0, len(align.s2.seq) ):
+            s += "{} ".format(align.s2.seq[j])
+        s += '\n'
+
+        for r in range(0, len(align.i1) + 1):
+            j = 0
+            if r < len(align.s2.seq):
+                s += '{} '.format(align.s1.seq[r-1])
+
+            for c in range(0, len(align.i2) + 2):
+                s += "{} ".format(align.smat[r][c])
+            s += "\n"
+
+        s += '\npath\n\n'
+        for r in range(0, len(align.i1) + 2):
+            j = 0
+            print()
+            for c in range(0, len(align.i2) + 2):
+                s += "{} ".format(align.pmat[r][c])
+            s += "\n"
+
+        return s
+
 
 # ==================================================================================================
 # main/test
@@ -54,8 +87,14 @@ if __name__ == '__main__':
     align.gi = 0
     align.gd = -1
 
-    smat = [[0 for c in range(len(align.i2))] for r in range(len(align.i1))]
-    pmat = [[0 for c in range(len(align.i2))] for r in range(len(align.i1))]
+    # three pointer directions
+    d = 1
+    h = 2
+    v = 4
+
+    # score and path matrix are sequence length + 2
+    align.smat = [[0 for c in range(len(align.i2) + 2)] for r in range(len(align.i1) + 2)]
+    align.pmat = [[0 for c in range(len(align.i2) + 2)] for r in range(len(align.i1) + 2)]
 
     # fill first row (including end gap penalties)
     r = 0
@@ -65,16 +104,57 @@ if __name__ == '__main__':
     i2 = align.i2
     gi = align.gi
     gd = align.gd
+    smat = align.smat
+    pmat = align.pmat
 
-    smat[r][c] = s[i1[r]][i2[c]]
-    for c in range(1, len(align.i2)):
-        smat[r][c] = s[i1[r]][i2[c]] + gi + c * gd
+    # top and left edge conditions (end gaps)
+    for c in range(1, len(align.i2) + 2):
+        smat[r][c] = gi + c * gd
+        pmat[r][c] = h
+    c = 0
+    for r in range(1, len(align.i1) + 2):
+        smat[r][c] = gi + r * gd
+        pmat[r][c] = v
 
-    for r in range(1,len(align.i1)):
-        c = 0
-        smat[r][c] = s[i1[r]][i2[c]] + gi + r * gd
-        for c in range(1, len(align.i2)):
-            smat[r][c] = s[i1[r]][i2[c]]
+    # body of comparison
+    i = 0
+    for r in range(1, len(align.i1) + 1):
+        j = 0
+        print()
+        for c in range(1, len(align.i2) + 1):
 
+            diag = smat[r - 1][c - 1] + s[i1[i]][i2[j]]
+            left = smat[r][c - 1] + gd
+            up = smat[r - 1][c] + gd
+            best = max(diag, left, up)
+            smat[r][c] = best
+            print('r:{} c:{} i:{} j:{} s:{},{},{} diag:{} left:{} up:{} best:{}'.format(
+                r, c, i, j, i1[i], i2[j], s[i1[i]][i2[j]], diag, left, up, best))
+
+            # set pointers
+            if diag == best:
+                pmat[r][c] += d
+            if left == best:
+                pmat[r][c] += h
+            if up == best:
+                pmat[r][c] += v
+
+            j += 1
+
+        i += 1
+
+    # final edge conditions (end gaps on right)
+    r = len(align.i1) + 1
+    end = len(align.i2)
+    for c in range(1, len(align.i2) + 2):
+        smat[r][c] = smat[r - 1][c - 1] + gi + (end - c) * gd
+        pmat[r][c] = d
+    c = len(align.i2) + 1
+    end = len(align.i1)
+    for r in range(1, len(align.i1) + 2):
+        smat[r][c] = smat[r - 1][c - 1] + gi + (end - r) * gd
+        pmat[r][c] = d
+
+    print(align.matString())
 
 exit(0)
