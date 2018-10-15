353a9a57802e385f5daf15e79b4a4e7ed2a2380e
Michael Gribskov <mgribsko@purdue.edu>
2018-10-15 09:20:43 -0400

    scaffold0 - trying  functions for reading bam

--- scaffold/scaffold0.py
+++ scaffold/scaffold0.py
@@ -11,14 +11,34 @@ import pysam
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     bamfile = sys.argv[1]
-    sys.stderr.write('bam file: {}'.format(bamfile))
+    sys.stderr.write('bam file: {}\n'.format(bamfile))
 
     bam = pysam.AlignmentFile(bamfile, 'rb')
+
+    # ref = {}
+    # nref = 0
+    # for i in range(len(bam.references)):
+    #     ref[bam.references[i]] = bam.lengths[i]
+    #     print( 'ref:{}     {}'.format(bam.references[i], bam.lengths[i]))
+    #     nref += 1
+    #     if nref > 99:
+    #         break
+
+    sys.stderr.write('references: {}\n'.format(bam.nreferences))
+
     nbam = 0
-    for read in bam:
-        nbam += 1
-        print(read)
-        if nbam > 4:
+    nref = 0
+    for ref in bam.references:
+        nref += 1
+        if nref > 4:
             break
+
+        p = 0
+        print(ref)
+        for pos in bam.pileup(ref):
+            p += 1
+            print(pos.n)
+            if p > 5:
+                break
 
 exit(0)
