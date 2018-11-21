a5b3871cd5d1fca3f388cb570b14fa817eb232b5
gribskov <mgribskov@gmail.com>
2018-11-21 08:09:12 -0500

    fastq_reverse - basic shell for reading entries

--- utils/fastq_reverse.py
+++ utils/fastq_reverse.py
@@ -29,11 +29,11 @@ def read_fastq(fh):
 
 if __name__ == '__main__':
     f1 = sys.argv[1]
+    fout = f1 + '.reverse'
 
-    sys.stdout.write('fastq_trimlength\n')
-    sys.stdout.write('     length: {}\n'.format(L))
-    sys.stdout.write('     fastq 1: {}\n'.format(f1))
-    sys.stdout.write('     fastq 2: {}\n'.format(f2))
+    sys.stdout.write('fastq_reverse\n')
+    sys.stdout.write('     fastq input: {}\n'.format(f1))
+    sys.stdout.write('     fastq output: {}\n'.format(fout))
 
     # open input files]
     r1 = None
@@ -43,20 +43,17 @@ if __name__ == '__main__':
         sys.stderr.write('Unable to open fastq 1 ({})\n'.format(f1))
 
     # open output files
-    out1 = f1 + '.clipped'
     o1 = None
     try:
-        o1 = open(out1, 'w')
+        o1 = open(fout, 'w')
     except IOError:
-        sys.stderr.write('Unable to open output fastq 1 ({})\n'.format(out1))
+        sys.stderr.write('Unable to open output fastq 1 ({})\n'.format(fout))
 
     nread = 0
     nwritten = 0
-    ndropped = 0
-    limit = L + 1  # sequence strings have a newline so add one
     while True:
-        fq1 = read_fastq(r1)
-        if fq1['title'] == '' or fq2['title'] == '':
+        fastq = read_fastq(r1)
+        if fastq['title']:
             break
 
         nread = nread + 1
@@ -64,26 +61,9 @@ if __name__ == '__main__':
         #     nread -= 1
         #     break
         if not nread % 1000000:
-            sys.stderr.write('\n{}\t{}\t{}'.format(nread, nwritten, ndropped))
-
-        if (len(fq1['seq']) < limit) or (len(fq2['seq']) < limit):
-            ndropped += 1
-            # print(len(fq1['seq']), fq1['seq'])
-            # print(len(fq2['seq']), fq2['seq'])
-        else:
-            fq1['seq'] = fq1['seq'][:L]
-            fq2['seq'] = fq2['seq'][:L]
-            fq1['qual'] = fq1['qual'][:L]
-            fq2['qual'] = fq2['qual'][:L]
-
-            nwritten += 1
-            o1.write('{}\n{}\n{}\n{}\n'.format(fq1['title'].rstrip(), fq1['seq'].rstrip(),
-                                               fq1['sep'].rstrip(), fq1['qual'].rstrip()))
-            o2.write('{}\n{}\n{}\n{}\n'.format(fq2['title'].rstrip(), fq2['seq'].rstrip(),
-                                               fq2['sep'].rstrip(), fq2['qual'].rstrip()))
+            sys.stderr.write('\n{}\t{}\n'.format(nread, nwritten))
 
     sys.stdout.write('\n{} reads read\n'.format(nread))
-    sys.stdout.write('{} reads written to {} and {}\n'.format(nwritten, out1, out2))
-    sys.stdout.write('{} reads dropped\n'.format(ndropped))
+    sys.stdout.write('{} reads written to {}\n'.format(nwritten, fout))
 
 exit(0)
