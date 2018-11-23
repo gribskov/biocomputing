6367f963f84b0623b5587d90919a9f4764be0660
Michael Gribskov <mgribsko@gmail.com>
2018-11-23 13:02:58 -0500

    fastq_reverse - functional version

--- utils/fastq_reverse.py
+++ utils/fastq_reverse.py
@@ -18,7 +18,7 @@ def fastq_read(fh):
     fq = {'title': fh.readline().rstrip(),
           'seq': fh.readline().rstrip(),
           'sep': fh.readline().rstrip(),
-          'qual': fh.readline().rstrip() }
+          'qual': fh.readline().rstrip()}
     # fq['title'] = fh.readline()
     # fq['seq'] = fh.readline()
     # fq['sep'] = fh.readline()
@@ -26,20 +26,38 @@ def fastq_read(fh):
 
     return fq
 
+
 def fastq_write(fastq, fh):
     """---------------------------------------------------------------------------------------------
 
-    :param fastq:
-    :param fh:
+    :param fastq: dictionary with fastq information: keys - title, seq, sep, qual
+    :param fh: output filehandle
     :return:
     ---------------------------------------------------------------------------------------------"""
+    fh.write('{}\n'.format(fastq['title']))
+    fh.write('{}\n'.format(fastq['seq']))
+    fh.write('{}\n'.format(fastq['sep']))
+    fh.write('{}\n'.format(fastq['qual']))
+
+    return
+
+
 def sequence_revcomp(seq):
-    """-----------------------------------------------------------------------------------------
+    """--------------------------------------------------------------------------------------------
     reverse complement the sequence
     :param seq:
-    :return: string, reverse complement of sequence
-    -----------------------------------------------------------------------------------------"""
+    :return: string, reverse complement of sequence--
+    ---------------------------------------------------------------------------------------------"""
     return seq.translate(complement)[::-1]
+
+
+def quality_rev(qual):
+    """---------------------------------------------------------------------------------------------
+    Reverse the quality vector to correspond to reverse complement of sequence
+    :param qual: list of ascii quality scores
+    :return: list (reversed)
+    ---------------------------------------------------------------------------------------------"""
+    return qual[::-1]
 
 
 # --------------------------------------------------------------------------------------------------
@@ -55,14 +73,14 @@ if __name__ == '__main__':
     sys.stdout.write('     fastq input: {}\n'.format(f1))
     sys.stdout.write('     fastq output: {}\n'.format(fout))
 
-    # open input files]
+    # open input file
     r1 = None
     try:
         r1 = open(f1, 'r')
     except IOError:
         sys.stderr.write('Unable to open fastq 1 ({})\n'.format(f1))
 
-    # open output files
+    # open output file
     o1 = None
     try:
         o1 = open(fout, 'w')
@@ -75,12 +93,15 @@ if __name__ == '__main__':
         fastq = fastq_read(r1)
         if not fastq['title']:
             break
-        print('seq:', fastq['seq'])
-        print('rev:', sequence_revcomp(fastq['seq']))
+
+        nwritten += 1
+        fastq['seq'] = sequence_revcomp(fastq['seq'])
+        fastq['qual'] = quality_rev(fastq['qual'])
+        fastq_write(fastq, o1)
 
         nread = nread + 1
         if nread > 10:
-           break
+            break
 
         if not nread % 1000000:
             sys.stderr.write('\n{}\t{}\n'.format(nread, nwritten))
