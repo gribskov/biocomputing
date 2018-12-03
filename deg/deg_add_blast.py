b106b4ffccc3152d9f219aaafb7f6d7b7e705981
Michael Gribskov <mgribsko@purdue.edu>
2018-12-03 08:04:24 -0500

    deg_add_blast - added reading of deg file

--- deg/deg_add_blast.py
+++ deg/deg_add_blast.py
@@ -14,26 +14,44 @@ if __name__ == '__main__':
 
     topmax = 3
     blastfile = sys.argv[1]
+    degfile = sys.argv[2]
+
     sys.stderr.write('deg_add_blast')
     sys.stderr.write('\tBlast file: {}\n'.format(blastfile))
+    sys.stderr.write('\tDEG file: {}\n'.format(degfile))
 
+    # open blastfile right away in case of error
     blast = Blast()
     blast.new(blastfile)
-
     # format (diamond)
     nfields = blast.setFormat('qid qlen qbegin qend sid slen sbegin send len pid evalue doc')
 
-    # TODO read in and store DEG file
+    # read in and store DEG file
+    try:
+        deg = open(degfile, 'r')
+    except:
+        sys.stderr.write('Unable to open DEG file ({})\n'.format(degfile))
+        exit(1)
+
+    ndeg = 0
+    deginfo = {}
+    for line in deg:
+        # line = line.rstrip()
+        ndeg += 1
+        id, info = line.rstrip().split('\t', maxsplit=1)
+        deginfo[id] = info
+
+    sys.stderr.write('{} genes read from {}\n'.format(ndeg, degfile))
 
     # read blast file, skipping any ID not in DEG file
     # TODO add logic for skipping
 
-
-    n=0
+    n = 0
     top = 1
     blast.read()
     query = blast.qid
-    info = '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin,blast.qend,blast.sbegin,blast.send,blast.evalue,blast.doc)
+    info = '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin, blast.qend, blast.sbegin, blast.send,
+                                               blast.evalue, blast.doc)
     while blast.next():
 
         if blast.qid == query:
@@ -48,7 +66,8 @@ if __name__ == '__main__':
             info = ''
 
         if top <= topmax:
-            info += '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin,blast.qend,blast.sbegin,blast.send,blast.evalue,blast.doc)
+            info += '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin, blast.qend, blast.sbegin,
+                                                        blast.send, blast.evalue, blast.doc)
 
         if n > 5:
             break
