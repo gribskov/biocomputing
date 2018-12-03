23b2f54c9f05e79d7b9bd21c5b6a252de3475f2d
Michael Gribskov <mgribsko@purdue.edu>
2018-12-03 05:42:02 -0500

    deg_add_blast - basic reading of blast file ok

--- deg/deg_add_blast.py
+++ deg/deg_add_blast.py
@@ -12,6 +12,7 @@ from blast.blast import Blast
 # ==================================================================================================
 if __name__ == '__main__':
 
+    topmax = 3
     blastfile = sys.argv[1]
     sys.stderr.write('deg_add_blast')
     sys.stderr.write('\tBlast file: {}\n'.format(blastfile))
@@ -20,15 +21,36 @@ if __name__ == '__main__':
     blast.new(blastfile)
 
     # format (diamond)
-
     nfields = blast.setFormat('qid qlen qbegin qend sid slen sbegin send len pid evalue doc')
-    for f in blast.fields:
-        print('        ', f, '\t', getattr(blast, f))
+
+    # TODO read in and store DEG file
+
+    # read blast file, skipping any ID not in DEG file
+    # TODO add logic for skipping
+
 
     n=0
+    top = 1
+    blast.read()
+    query = blast.qid
+    info = '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin,blast.qend,blast.sbegin,blast.send,blast.evalue,blast.doc)
     while blast.next():
-        n += 1
-        print('   ', n, blast.line)
-        print('   {}\t{}\t{}'.format(n, blast.sid, blast.evalue))
+
+        if blast.qid == query:
+            top += 1
+
+        else:
+            sys.stderr.write('{}\t{}'.format(query, info))
+            n += 1
+            query = blast.qid
+
+            top = 1
+            info = ''
+
+        if top <= topmax:
+            info += '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin,blast.qend,blast.sbegin,blast.send,blast.evalue,blast.doc)
+
+        if n > 5:
+            break
 
 exit(0)
