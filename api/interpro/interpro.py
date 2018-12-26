ce6ad980cea04939450a993fd1360c4ac61e4a26
Michael Gribskov <mgribsko@purdue.edu>
2018-12-26 13:32:12 -0500

    interpro - committing for safety, working on error messages

--- api/interpro/interpro.py
+++ api/interpro/interpro.py
@@ -1,11 +1,14 @@
+import sys
+import time
+import requests
+
+
 class Interpro:
     """=============================================================================================
     Interpro class for running interproscan
 
     25 December 2018    Michael Gribskov
     ============================================================================================="""
-    import sys
-    import requests
 
     def __init__(self, loglevel=0, poll_time=60, poll_count=20):
         """-----------------------------------------------------------------------------------------
@@ -14,7 +17,7 @@ class Interpro:
         track   0 no log, 1 job submission/completion, 2 all
         -----------------------------------------------------------------------------------------"""
         self.log = loglevel
-        self.commands_avail = {'run', 'status', 'result'}
+        self.commands_avail = ['run', 'status', 'result']
         self.output_avail = {'xml', 'json', 'tsv', 'out', 'gff', 'svg'}
         self.output = 'tsv'
         self.poll_time = poll_time  # seconds between polling
@@ -27,7 +30,7 @@ class Interpro:
         self.url = 'https://www.ebi.ac.uk/Tools/services/rest/iprscan5/'
         self.jobid = ''
         self.state = 'UNKNOWN'
-        self.result = ''
+        self.content = ''
 
     def run(self):
         """-----------------------------------------------------------------------------------------
@@ -39,14 +42,20 @@ class Interpro:
         param = {'email': self.email, 'title': self.title, 'sequence': self.sequence}
         command = self.url + 'run'
         response = requests.post(command, param)
-        self.jobid = response.text
-        # TODO add error trapping
+        if response.status_code == 200:
+            # success
+            self.jobid = response.text
+        else:
+        # failure
+
+        #  TODO add error trapping
 
         if self.log:
-            # TODO add time and sequence name
-            sys.stderr.write('job {} submitted\n'.format(self.jobid))
+            # TODO add sequence name?
+            sys.stderr.write(
+                '{}\tinterproscan job {} submitted\n'.format(Interpro.logtime(), self.jobid))
 
-        return jobid
+        return self.jobid
 
     def status(self):
         """-----------------------------------------------------------------------------------------
@@ -54,6 +63,7 @@ class Interpro:
 
         :return: Logical, True if a result was returned
         -----------------------------------------------------------------------------------------"""
+        response = None
         complete = False
         tries = 0
         while not complete:
@@ -61,11 +71,11 @@ class Interpro:
             time.sleep(self.poll_time)
             tries += 1
 
-            command = self.url + self.jobid
+            command = self.url + 'status/' + self.jobid
             response = requests.get(command)
             if self.log > 1:
-                # TODO add time and jobid
-                sys.stderr.write('    polling... response->{}\n'.format(response.text))
+                sys.stderr.write('{}\tinterproscan job{} polling - {}\n'.format(
+                    Interpro.logtime(), self.jobid, response.text))
 
             if 'FINISHED' in response.text:
                 complete = True
@@ -76,15 +86,14 @@ class Interpro:
         self.state = response.text
         if not complete:
             # polling reached limit
-            # TODO add time
+            if self.log > 0:
+                sys.stderr.write('{}\tinterproscan job {} unsuccessful after {} tries\n'.format(
+                    Interpro.logtime(), self.jobid, self.poll_count))
+
+        else:
             if self.log > 0:
                 sys.stderr.write(
-                    ('unable to find result () in {} tries\n'.format(self.jobid, self.poll_count))
-
-                else:
-                # TODO add time
-                if self.log > 0:
-                    sys.stderr.write('interproscan {} finished\n'.format(self.jobid))
+                    '{}\tinterproscan job {} finished\n'.format(Interpro.logtime(), self.jobid))
 
         return complete
 
@@ -92,31 +101,70 @@ class Interpro:
         """-----------------------------------------------------------------------------------------
         Retrieve the result
 
-        :return:
+        :return: Logical True=success, False=failure
         -----------------------------------------------------------------------------------------"""
         # get the final result
-        command = '{}{}/{}/{}'.format(self.url, 'result', self.jobid, self.output)
+        command = self.url + 'result/' + self.jobid + '/' + self.output
         response = requests.get(command)
-        self.result = response.text
-        # TODO error trapping
+        if response.status_code == 200:
+            # success
+            self.content = response.text
+            if self.log > 1:
+                sys.stderr.write('{}\tinterproscan job {} result retrieved from {} as {}'.format(
+                    Interpro.logtime(), self.jobid, self.url, self.output))
+            return True
+
+        else:
+            # failure
+            sys.stderr.write('{}\t{}interproscan job {} error retrieving result\tstatus={}'.format(
+                Interpro.logtime(), self.jobid, response.status_code))
 
         return
+
+    @classmethod
+    def logtime(cls):
+        """-----------------------------------------------------------------------------------------
+        Return current time as a string. Format is 10/Oct/2000:13:55:36 which is similar to the
+        common log format (without the time zone)
+
+        :return: string
+        -----------------------------------------------------------------------------------------"""
+        return time.strftime('%d/%b/%G:%H:%M:%S', time.localtime(time.time()))
+
+    def response_is_error(self, task, response):
+        """-----------------------------------------------------------------------------------------
+        Return true if the response code is other than 200. Write error message to stderr if
+        loglevel > 1. Task is a string describing the task that failed for inclusion in the error message
+
+        :param response: requests object response
+        :return: logical True = error, False = no error
+        -----------------------------------------------------------------------------------------"""
+        is_error = False
+        if response.status_code == 200:
+        # success
+        else:
+            if self.log > 0:
+                sys.stderr.write('{}\t{}interproscan job {} error {}\tstatus={}'.format(
+                    Interpro.logtime(), self.jobid, task, response.status_code))
+            is_error = True
+
+        return is_error
 
 
 # ==================================================================================================
 # Testing
 # ==================================================================================================
 if __name__ == '__main__':
-    ips = Interpro()
+    ips = Interpro(loglevel=2, poll_time=20)
     ips.email = 'gribskov@purdue.edu'
-    ipos.title = 'globin'
+    ips.title = 'globin'
     ips.sequence = '''>sp|P69905.2|HBA_HUMAN RecName: Full=Hemoglobin subunit alpha; AltName: Full=Alpha-globin; AltName: Full=Hemoglobin alpha chain
     MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNA
     VAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSK
     YR'''
 
     ips.run()
-    if ips.status:
+    if ips.status():
         ips.result()
 
 exit(0)
