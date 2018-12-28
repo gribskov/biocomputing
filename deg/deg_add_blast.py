e742751423b951959f1103809992aa4b9983b727
Michael Gribskov <mgribsko@purdue.edu>
2018-12-28 12:49:07 -0500

    deg_add_blast - committed current updates for teesting

--- deg/deg_add_blast.py
+++ deg/deg_add_blast.py
@@ -15,10 +15,17 @@ if __name__ == '__main__':
     topmax = 3
     blastfile = sys.argv[1]
     degfile = sys.argv[2]
+    outfile = 'merge.tsv'
 
-    sys.stderr.write('deg_add_blast')
+    sys.stderr.write('deg_add_blast\n')
     sys.stderr.write('\tBlast file: {}\n'.format(blastfile))
     sys.stderr.write('\tDEG file: {}\n'.format(degfile))
+    sys.stderr.write('\tOutput file: {}\n'.format(outfile))
+
+    try:
+        out = open(outfile, 'w')
+    except:
+        sys.stderr.write('Error opening output file ({})\n'.format(outfile))
 
     # open blastfile right away in case of error
     blast = Blast()
@@ -35,41 +42,54 @@ if __name__ == '__main__':
 
     ndeg = 0
     deginfo = {}
+    header = deg.readline()
     for line in deg:
         # line = line.rstrip()
         ndeg += 1
-        id, info = line.rstrip().split('\t', maxsplit=1)
+        id, info = line.rstrip().replace('"', '').split('\t', maxsplit=1)
         deginfo[id] = info
 
-    sys.stderr.write('{} genes read from {}\n'.format(ndeg, degfile))
+    sys.stderr.write('\n{} genes read from {}\n'.format(ndeg, degfile))
 
     # read blast file, skipping any ID not in DEG file
-    # TODO add logic for skipping
 
-    n = 0
+    nblast = 0
+    nfound = 0
     top = 1
     blast.read()
     query = blast.qid
-    info = '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin, blast.qend, blast.sbegin, blast.send,
-                                               blast.evalue, blast.doc)
+    info = '{}\t{}\t{}\t{}\t{}\t{}'.format(blast.qbegin, blast.qend, blast.sbegin, blast.send,
+                                           blast.evalue, blast.doc)
     while blast.next():
 
         if blast.qid == query:
+            # another line of the current query
             top += 1
 
         else:
-            sys.stderr.write('{}\t{}'.format(query, info))
-            n += 1
+            # a new query, save the old query top lines
+            if query in deginfo:
+                nfound += 1
+                out.write('{}\t{}\t"{}"\n'.format(query, deginfo[query], info))
+            nblast += 1
             query = blast.qid
+            info = '{}\t{}\t{}\t{}\t{}\t{}'.format(blast.qbegin, blast.qend, blast.sbegin,
+                                                   blast.send,
+                                                   blast.evalue, blast.doc)
 
             top = 1
             info = ''
 
         if top <= topmax:
-            info += '\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(blast.qbegin, blast.qend, blast.sbegin,
-                                                        blast.send, blast.evalue, blast.doc)
+            info += '\n{}\t{}\t{}\t{}\t{}\t{}'.format(blast.qbegin, blast.qend, blast.sbegin,
+                                                      blast.send, blast.evalue, blast.doc)
+        # if nfound > 5:
+        #     break
 
-        if n > 5:
-            break
+    if query in deginfo:
+        nfound += 1
+        out.write('{}\t{}\t"{}"\n'.format(query, deginfo[query], info))
+
+    sys.stderr.write('{} genes found in blast result {}\n'.format(nfound, blastfile))
 
 exit(0)
