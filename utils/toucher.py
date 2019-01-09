94da828e21d9505dfb1d430f76ec8e88928b9fe0
Michael Gribskov <mgribsko@purdue.edu>
2019-01-09 06:55:39 -0500

    toucher - directory walk and time calculation done, implementing search for target directories

--- utils/toucher.py
+++ utils/toucher.py
@@ -8,6 +8,15 @@ import sys
 import os
 import time
 
+
+def find_target_directories():
+    """---------------------------------------------------------------------------------------------
+    # TODO function to identify directories that need updating
+    ---------------------------------------------------------------------------------------------"""
+    pass
+    return
+
+
 roots = []
 
 if len(sys.argv) > 1:
@@ -17,43 +26,58 @@ else:
     # if no starting point is provided use the current working directory
     roots.append('.')
 
-purgeday = 60
-purgesec = int(0.8 * purgeday * 24 * 3600)
-print( 'purge time: {} s'.format(purgesec))
+purgeday = 60  # purge limit in days, set by RCAC
+purgesec = purgeday * 24 * 3600  # purge limit in seconds
+targetsec = int(
+    0.5 * purgeday * 24 * 3600)  # target after access time adjustment, 50% of purge limit
+print('purge limit: {} day {} sec'.format(purgeday, purgesec))
+print('purge target: {} s'.format(purgesec))
 
 for root in roots:
     for thisdir, dirlist, filelist in os.walk(root):
-        print('Found directory: {}'.format(thisdir))
+        print('\ndirectory: {}'.format(thisdir))
         now = time.time()
         oldest = now
+        newest = 0
         oldest_file = ''
         times = {}
         for file in filelist:
             # find the oldest file
-            full_file = os.path.join(thisdir,file)
+            full_file = os.path.join(thisdir, file)
             times[full_file] = os.stat(full_file).st_atime
             if times[full_file] < oldest:
                 oldest_file = full_file
                 oldest = times[full_file]
-        print('oldest file: {}\t{}\t{}\t{}'.format(oldest_file, times[oldest_file], now, int(now-times[oldest_file])))
+            if times[full_file] > newest:
+                newest = times[full_file]
+        # print('oldest file: {}\t{}\t{}\t{}'.format(oldest_file, times[oldest_file], now, int(now-times[oldest_file])))
+
+        scale = (newest - oldest) / targetsec
+        offset = (1 - scale) * newest
 
         for file in filelist:
-            print('\t{}'.format(file))
-            file = os.path.join(thisdir, file)
-            print('\t{}'.format(file))
+            # print('\t{}'.format(file))
+            full_file = os.path.join(thisdir, file)
+            # print('\t{}'.format(file))
 
-            stat = os.stat(file)
-            print(stat)
-            print('size: {}'.format(time.ctime(stat.st_size)))
-            print('accessed: {}'.format(time.ctime(stat.st_atime)))
-            print('modified: {}'.format(time.ctime(stat.st_mtime)))
-            print('changed: {}'.format(time.ctime(stat.st_ctime)))
+            stat = os.stat(full_file)
+            newaccess = stat.st_atime * scale + offset
 
-            # os.utime(file, (stat.st_atime+3600, stat.st_mtime))
+            # print(stat)
+            print('accessed: {} -> {}\t{}\t{}\t\t{}\t{}'.format(
+                time.ctime(stat.st_atime),
+                time.ctime(newaccess),
+                file,
+                stat.st_size,
+                time.ctime(stat.st_mtime),
+                time.ctime(stat.st_ctime)))
 
-            stat = os.stat(file)
-            print(stat)
-            print('size: {}'.format(time.ctime(stat.st_size)))
-            print('accessed: {}'.format(time.ctime(stat.st_atime)))
-            print('modified: {}'.format(time.ctime(stat.st_mtime)))
-            print('changed: {}'.format(time.ctime(stat.st_ctime)))
+            # TODO turn on to actually change access time
+            os.utime(full_file, (stat.st_atime + 3600, stat.st_mtime))
+
+            # stat = os.stat(full_file)
+            # print(stat)
+            # print('size: {}'.format(time.ctime(stat.st_size)))
+            # print('accessed: {}'.format(time.ctime(stat.st_atime)))
+            # print('modified: {}'.format(time.ctime(stat.st_mtime)))
+            # print('changed: {}'.format(time.ctime(stat.st_ctime)))
