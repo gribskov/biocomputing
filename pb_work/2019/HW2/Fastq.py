9a838322c2ed9f50179b5eb28cc29989de0feff3
Michael Gribskov <mgribsko@gmail.com>
2019-02-03 15:24:18 -0500

    Fastq - step 0

---
+++ pb_work/2019/HW2/Fastq.py
@@ -0,0 +1,30 @@
+class Fastq():
+    """=============================================================================================
+    FastQ sequence data class. Data is assumed to be available in a file.
+
+    Michael Gribskov     03 February 2019
+    ============================================================================================="""
+
+
+    def __init__(self):
+        """-----------------------------------------------------------------------------------------
+        FAstq object has attributes
+        filename - filename of data file
+        fh - filehandle of opend file
+        id - ID of current entry
+        sequence - sequence of current entry (string)
+        quality - quality of current entry (list of int)
+        -----------------------------------------------------------------------------------------"""
+        self.filename = ''
+        self.fh = None
+        self.id = ''
+        self.sequence = ''
+        self.quality = []
+
+
+# --------------------------------------------------------------------------------------------------
+# testing
+# --------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    exit(0)
