86eb140017fecb4dd972cb16b36a81954618a748
Michael Gribskov <mgribsko@gmail.com>
2019-02-03 20:20:39 -0500

    Fastq- step 4 add next

--- pb_work/2019/HW2/Fastq.py
+++ pb_work/2019/HW2/Fastq.py
@@ -36,6 +36,52 @@ class Fastq():
 
         return self.fh
 
+    def next(self):
+        """-----------------------------------------------------------------------------------------
+        read the next entry from the file.  Return false whten all entries have been read
+
+        :return: logical, false when done
+        -----------------------------------------------------------------------------------------"""
+
+        id = self.fh.readline()
+        self.id = id.rstrip()
+
+        seq = self.fh.readline()
+        self.sequence = seq.rstrip()
+
+        plus = self.fh.readline()
+        qual = self.fh.readline()
+        self.quality = self.get_quality_list(qual)
+
+        return True
+
+    def get_quality_list(self, qual):
+        """---------------------------------------------------------------------------------------------
+        Convert the quality string to a list of integer quality values.
+
+        The ASCII characters in the quality line represent the probability that the base call at the
+        corresponding position in the sequenceis incorrect. The are obtained as follows
+
+        Q = int( -10 * log10( P(error) ) )
+
+        Character = chr( 33 + Q )
+
+        Q is referred to as the Phred quality, or more often as simply quality . The character '5',
+        which is ASCII 53, represents quality = 20, which means an error probability of 10-2 or 0.01.
+        A conventional choice for the minimum permissible quality is 20.
+
+        Usage
+            quality = get_quality_list(qual)
+
+        :param qual: string
+        :return: list of int
+        ---------------------------------------------------------------------------------------------"""
+        for qval in qual:
+            q = ord(qval) - 33
+            self.quality.append(q)
+
+        return self.quality
+
 
 # --------------------------------------------------------------------------------------------------
 # testing
@@ -45,7 +91,17 @@ if __name__ == '__main__':
     fq = Fastq()
 
     fh = fq.openfile(fastqname)
-    print('file: {}'.format(fq.filename))
-    print('filehandle: {}'.format(fh))
+
+    n_entry = 0
+    while fq.next():
+        n_entry += 1
+
+        print('\t{}\t{}'.format(n_entry, fq.id))
+        print('\t{}'.format(fq.sequence))
+
+        if n_entry > 4:
+            break
+
+    # end of loop of Fastq entries
 
     exit(0)
