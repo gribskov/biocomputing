5cf14f2db54309fba1fefb75defad89961c00348
Michael Gribskov <mgribsko@gmail.com>
2019-02-09 11:47:51 -0500

    Point - Polar subclass

--- pb_work/examples/point.py
+++ pb_work/examples/point.py
@@ -24,27 +24,57 @@ class Point:
         return self._pos[:]
 
 
+class Polar(Point):
+    """ _pos = [angle, radius]"""
+
+    # Note: __init__ inherited from Point
+
+    def magnitude(self):
+        return self._pos[1]
+
+    def to_cartesian(self):
+        return self._pos[1] * math.cos(math.radians(self._pos[0])), \
+                self._pos[1] * math.sin(math.radians(self._pos[0]))
+
+
+    def add(self, other):
+        x1, y1 = self.to_cartesian()
+        x2, y2 = other.to_cartesian()
+
+        self._pos[0] = math.degrees(math.atan2(y1 + y2, x1 + x2))
+        self._pos[1] = math.sqrt((x1 + x2) ** 2 + (y1 + y2) ** 2)
+
+        return self._pos[1]
+
+
 # --------------------------------------------------------------------------------------------------
 #
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    pt1 = Point(1, 2)
-    pt2 = Point(4, 4)
+    po1 = Polar(0.0, 1.0)
+    print('po1 values = {}'.format(po1.values()))
+    po2 = Polar(90.0, 1.0)
+    print('po2 values = {}'.format(po2.values()))
+    po1.add(po2)
+    print('po1 + po2 = {}'.format(po1.values()))
 
-    print('pt1 length = {}'.format(pt1.magnitude()))
-    print('pt2 length = {}'.format(pt2.magnitude()))
+    # pt1 = Point(1, 2)
+    # pt2 = Point(4, 4)
 
-    length = pt1.add(pt2)
-    print('pt1 + pt2 = {}'.format(length))
-
-    x, y = pt1.values()
-    print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), x, y))
-
-    xy = []
-    xy = pt1.values()
-    print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), xy[0], xy[1]))
-    xy[0] = 10
-    xy = pt1.values()
-    print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), xy[0], xy[1]))
+    # print('pt1 length = {}'.format(pt1.magnitude()))
+    # print('pt2 length = {}'.format(pt2.magnitude()))
+    #
+    # length = pt1.add(pt2)
+    # print('pt1 + pt2 = {}'.format(length))
+    #
+    # x, y = pt1.values()
+    # print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), x, y))
+    #
+    # xy = []
+    # xy = pt1.values()
+    # print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), xy[0], xy[1]))
+    # xy[0] = 10
+    # xy = pt1.values()
+    # print('pt1 length = {}\tx = {}\ty = {}'.format(pt1.magnitude(), xy[0], xy[1]))
 
     exit(0)
