5bdafca7113d624adc676661746b0604a3f4b060
gribskov <gribskov@purdue.edu>
2019-10-14 14:51:00 -0400

    merge - moved bundle into object

--- gff/merge.py
+++ gff/merge.py
@@ -1,17 +1,25 @@
 from gff import Gff
 
-if __name__ == '__main__':
+class Bundle:
 
-    gff = Gff(file="stringtie.gff")
-    transcript_n = gff.read_feature('transcript')
-    print('{} features read'.format(transcript_n))
-    gff.replace_columns_re(['sequence'], 'lcl\|', r'')
+    def __init__(self):
+        """
 
-    bundle_n = 0
-    bundle = []
-    b_start = 0
+        """
+        self.begin = 0
+        self.end = 0
+        self.sequence = ''
+        self.transcript = []
+
+def make_bundle(gff):
+    """---------------------------------------------------------------------------------------------
+
+    :param gff: Gff object
+    :return: list of bundles
+    ---------------------------------------------------------------------------------------------"""
+    bundle_list = []
     b_stop = 0
-    for t in sorted(gff.data, key=lambda k: int(k['begin'])):
+    for t in sorted(gff.data, key=lambda k: (k['sequence'], int(k['begin']))):
         id = t['transcript_id']
         begin = int(t['begin'])
         end = int(t['end'])
@@ -20,18 +28,41 @@ if __name__ == '__main__':
 
         if begin > b_stop:
             # new bundle
-            bundle.append([t])
-
-            print('bundle {}'.format(bundle_n))
-            bundle_n += 1
-            b_start = begin
-            b_stop = end
+            b = Bundle()
+            b.begin = begin
+            b.end = end
+            b.sequence = sequence
+            b.transcript.append(t)
+            bundle_list.append(b)
 
         else:
             # continue current bundle
-            b_stop = end
-            bundle[-1].append([t])
+            b.stop = end
+            b.transcript.append(t)
 
-        print('\t{}\t{}\t{}\t{}\t{}'.format(id, sequence, begin, end, strand))
+        b_stop = end
+
+    return bundle_list
+
+if __name__ == '__main__':
+
+    gff = Gff(file="stringtie.gff")
+    transcript_n = gff.read_feature('transcript')
+    print('{} features read'.format(transcript_n))
+    gff.replace_columns_re(['sequence'], 'lcl\|', r'')
+
+    bundle = make_bundle(gff)
+    bundle_n = 0
+    print('{} bundles found'.format(len(bundle)))
+    for b in bundle:
+        print('\nbundle {}'.format(bundle_n))
+        bundle_n += 1
+        for transcript in b.transcript:
+            id = transcript['transcript_id']
+            begin = int(transcript['begin'])
+            end = int(transcript['end'])
+            sequence = transcript['sequence']
+            strand = transcript['strand']
+            print('\t{}\t{}\t{}\t{}\t{}'.format(id, transcript['sequence'], transcript['begin'], transcript['end'], transcript['strand']))
 
     exit(0)
