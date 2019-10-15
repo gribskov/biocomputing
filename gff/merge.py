0a6e38430a207096bf31af09fc976fe205514371
gribskov <gribskov@purdue.edu>
2019-10-15 08:32:25 -0400

    merge - merge bundles into overlapping sets

--- gff/merge.py
+++ gff/merge.py
@@ -11,29 +11,32 @@ class Bundle:
         self.sequence = ''
         self.transcript = []
 
-def make_bundle(gff):
+def make_bundle(gff,id_column='transcript_id'):
     """---------------------------------------------------------------------------------------------
 
     :param gff: Gff object
+    :param id_column: name of id column in gff hash
     :return: list of bundles
     ---------------------------------------------------------------------------------------------"""
     bundle_list = []
     b_stop = 0
+    sequence_old = ''
     for t in sorted(gff.data, key=lambda k: (k['sequence'], int(k['begin']))):
-        id = t['transcript_id']
+        id = t[id_column]
         begin = int(t['begin'])
         end = int(t['end'])
         sequence = t['sequence']
         strand = t['strand']
 
-        if begin > b_stop:
+        if begin > b_stop or sequence != sequence_old:
             # new bundle
             b = Bundle()
             b.begin = begin
-            b.end = end
+            b.end = max(b.end,end)
             b.sequence = sequence
             b.transcript.append(t)
             bundle_list.append(b)
+            sequence_old = sequence
 
         else:
             # continue current bundle
@@ -46,23 +49,72 @@ def make_bundle(gff):
 
 if __name__ == '__main__':
 
-    gff = Gff(file="stringtie.gff")
-    transcript_n = gff.read_feature('transcript')
-    print('{} features read'.format(transcript_n))
-    gff.replace_columns_re(['sequence'], 'lcl\|', r'')
+    ugff = Gff(file="stringtie.gff")
+    transcript_n = ugff.read_feature('transcript')
+    print('{} unstranded features read'.format(transcript_n))
+    ugff.replace_columns_re(['sequence'], 'lcl\|', r'')
+    ubundle = make_bundle(ugff)
 
-    bundle = make_bundle(gff)
-    bundle_n = 0
-    print('{} bundles found'.format(len(bundle)))
-    for b in bundle:
-        print('\nbundle {}'.format(bundle_n))
-        bundle_n += 1
-        for transcript in b.transcript:
-            id = transcript['transcript_id']
-            begin = int(transcript['begin'])
-            end = int(transcript['end'])
-            sequence = transcript['sequence']
-            strand = transcript['strand']
-            print('\t{}\t{}\t{}\t{}\t{}'.format(id, transcript['sequence'], transcript['begin'], transcript['end'], transcript['strand']))
+    sgff = Gff(file="genome.gff")
+    sgff.attr_sep = '='
+    transcript_n = sgff.read_feature('mRNA')
+    print('{} stranded features read'.format(transcript_n))
+
+    # query = r'(maker|augustus|masked|processed|gene)(-|_)'
+    query = r'(maker|augustus|masked|processed|gene|trnascan)(-|_)'
+
+    sgff.replace_columns_re(['Parent', 'ID'], query, r'')
+    sgff.replace_columns_re(['Parent', 'ID'], r'-tRNA-', r'.tRNA')
+    sgff.rename_key('ID', 'transcript_id')
+    sbundle = make_bundle(sgff)
+    # sbundle = make_bundle(sgff, id_column='ID')
+
+
+    set = []
+    set_end = 0
+    set_seq = ''
+    for b in sorted(ubundle+sbundle, key=lambda k: (k.sequence, k.begin)):
+
+        if b.begin > set_end or b.sequence != set_seq:
+            # new set
+            set_seq = b.sequence
+            set_end = b.end
+            set.append( {'begin'   : b.begin,
+                        'end'     : set_end,
+                        'sequence': set_seq,
+                        'member'  : [b]} )
+
+        else:
+        # continue current set
+            set[-1]['end'] = max(set[-1]['end'], b.end)
+            set[-1]['member'].append(b)
+            set_end = set[-1]['end']
+
+    s_n = 0
+    for s in set:
+        print('set {}'.format(s_n))
+        s_n += 1
+        for b in s['member']:
+            print('\t{}\t{}\t{}'.format(b.sequence, b.begin, b.end))
+            for transcript in b.transcript:
+                    id = transcript['transcript_id']
+                    begin = int(transcript['begin'])
+                    end = int(transcript['end'])
+                    sequence = transcript['sequence']
+                    strand = transcript['strand']
+                    print('\t \t{}\t{}\t{}\t{}\t{}'.format(id, transcript['sequence'], transcript['begin'], transcript['end'], transcript['strand']))
+
+    # bundle_n = 0
+    # print('{} bundles found'.format(len(bundle)))
+    # for b in bundle:
+    #     print('\nbundle {}'.format(bundle_n))
+    #     bundle_n += 1
+    #     for transcript in b.transcript:
+    #         id = transcript['transcript_id']
+    #         begin = int(transcript['begin'])
+    #         end = int(transcript['end'])
+    #         sequence = transcript['sequence']
+    #         strand = transcript['strand']
+    #         print('\t{}\t{}\t{}\t{}\t{}'.format(id, transcript['sequence'], transcript['begin'], transcript['end'], transcript['strand']))
 
     exit(0)
