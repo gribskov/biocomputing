21d16b015af1b1fbb6fb08ecd81c47ad62d9be46
Michael Gribskov <mgribsko@gmail.com>
2021-01-15 13:21:14 -0500

    Partially working versio of orf that translates reading frame zero

---
+++ pb_work/examples/ORF/orf1.py
@@ -0,0 +1,49 @@
+"""=================================================================================================
+Given a DNA Sequence, what are all the open reading frames (ORF) longer than L
+    lengths in nucleotides
+    lengths in residues
+    amino acid sequence
+
+Michael Gribskov     15 January 2021
+================================================================================================="""
+codon2aa = {"AAA": "K", "AAC": "N", "AAG": "K", "AAT": "N",
+            "ACA": "T", "ACC": "T", "ACG": "T", "ACT": "T",
+            "AGA": "R", "AGC": "S", "AGG": "R", "AGT": "S",
+            "ATA": "I", "ATC": "I", "ATG": "M", "ATT": "I",
+
+            "CAA": "Q", "CAC": "H", "CAG": "Q", "CAT": "H",
+            "CCA": "P", "CCC": "P", "CCG": "P", "CCT": "P",
+            "CGA": "R", "CGC": "R", "CGG": "R", "CGT": "R",
+            "CTA": "L", "CTC": "L", "CTG": "L", "CTT": "L",
+
+            "GAA": "E", "GAC": "D", "GAG": "E", "GAT": "D",
+            "GCA": "A", "GCC": "A", "GCG": "A", "GCT": "A",
+            "GGA": "G", "GGC": "G", "GGG": "G", "GGT": "G",
+            "GTA": "V", "GTC": "V", "GTG": "V", "GTT": "V",
+
+            "TAA": "*", "TAC": "Y", "TAG": "*", "TAT": "T",
+            "TCA": "S", "TCC": "S", "TCG": "S", "TCT": "S",
+            "TGA": "*", "TGC": "C", "TGG": "W", "TGT": "C",
+            "TTA": "L", "TTC": "F", "TTG": "L", "TTT": "F"}
+# --------------------------------------------------------------------------------------------------
+# Main program
+# --------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    # Read nucleotide file
+    # for now just a dummy sequence string
+    seq = 'CATATCATCGATCTACGTACGTATCGAGCTATCGGACTCGATCGATCGATCGTCGATCGTAGCATGCTAGCTAGCTAG'
+
+    # check codon table
+
+    # Translate each reading frame and save ORFs
+
+    ncodon = 0
+    for codon in codon2aa:
+        ncodon += 1
+        print(codon, codon2aa[codon])
+
+    print('number of codons:', ncodon)
+
+    # Report results
+
+    exit(0)
