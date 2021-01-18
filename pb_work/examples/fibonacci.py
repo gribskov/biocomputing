0592bec0f63d875a407dcda141eb89a12a384d0d
Michael Gribskov <mgribsko@gmail.com>
2021-01-18 11:18:46 -0500

    fibonacci - complete working program

--- pb_work/examples/fibonacci.py
+++ pb_work/examples/fibonacci.py
@@ -9,11 +9,25 @@ Michael Gribskov     18 January 2021
 # main program
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+    # initialize n_max, the maximum term to calculate
+    n_max = 6
 
     # initialize the first two values, f and f_old
+    f_old = 0
+    f = 1
 
     # print the first two terms
+    print('F(', 0, ') =', f_old)
+    print('F(', 1, ') =', f)
 
     # for each successive term, up to n_max, print the new term
+    n = 2
+    while n < n_max:
+        f_new = f + f_old
+        print('F(', n, ') =', f_new)
+
+        f_old = f
+        f = f_new
+        n += 1
 
     exit(0)
