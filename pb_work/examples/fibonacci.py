82537b522721e7188dd3a3d47e55b81e61270822
Michael Gribskov <mgribsko@gmail.com>
2021-01-18 11:07:08 -0500

    fibonacci - added comments and header

--- pb_work/examples/fibonacci.py
+++ pb_work/examples/fibonacci.py
@@ -1,11 +1,19 @@
 """=================================================================================================
-
+Calculates and prints the values of the fibonacci series, F, up to max_n terms
+F(n) = F(n-1) + f(n-2)
 
 Michael Gribskov     18 January 2021
 ================================================================================================="""
 
 # --------------------------------------------------------------------------------------------------
-#
+# main program
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+
+    # initialize the first two values, f and f_old
+
+    # print the first two terms
+
+    # for each successive term, up to n_max, print the new term
+
     exit(0)
