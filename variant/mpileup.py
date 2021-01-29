9580767d22b27c3e5ad608ab0e0e8745ed101ab6
Michael Gribskov <mgribsko@gmail.com>
2021-01-29 10:35:37 -0500

    mpileup - adeed Hom/het/non-medelian/None genotype call based or arbitrary cutoffs

--- variant/mpileup.py
+++ variant/mpileup.py
@@ -73,16 +73,21 @@ class Mpileup:
         # indels = re.compile(r'[+-]([0-9]+)[ACGTNacgtn*#]{\g<1>}')
 
         bases = self.parsed['bases']
-        count = {'indel': [], 'mapqual': []}
+        count = {'indel': [], 'mapqual': [],
+                 'A': 0, 'a': 0, 'C': 0, 'c': 0, 'G': 0, 'g': 0, 'T': 0, 't': 0, 'N': 0, 'n': 0,
+                 '*': 0, '$': 0,
+                 'forward': 0, 'backward': 0, 'strand_bias': 0}
         indel_list = []
 
         # first check for multi-character sequences
+        begin = 0
         if '^' in bases:
             # beginning of sequence marker and map quality
             for match in readstart.finditer(bases):
                 # print(bases)
                 # print(match.group(1), ord(match.group(1)) - 33)
                 bases = readstart.sub('', bases)
+                begin += 1
 
         if '-' in bases or '+' in bases:
             # indel present
@@ -115,12 +120,53 @@ class Mpileup:
 
         # bias with +1 prior
         count['strand_bias'] = log((count['forward'] + 1) / (count['backward'] + 1), 2)
+        count['end'] = begin + count['$']
+        count['end_frac'] = (count['end'] + 1) / (self.parsed['depth'] + 1)
 
-        count['indel'] = indel_list
+        count['indel'] = len(indel_list) + count['*']
+        count['indel_frac'] = (count['indel'] + 1) / (self.parsed['depth'] + 1)
         self.count = count
         return self.count
 
     # end of count
+
+    def mendel(self, hommax=0.1, hetmin=0.4, hetmax=0.6):
+        """-----------------------------------------------------------------------------------------
+        Decides if a position is homozygous (H) or heterozygous (h).  Positions with less than
+        hommax alternate bases are H, positions with between hetmin and hetmax alternate bases are
+        h, anything els is N (non-mendelian).  If no base is present, an empty string is returned.
+
+        :param hommax: float, maximum alternate allele frequency for homozygous
+        :param hetmin: float, minimum alternate allele frequency for heterozygous
+        :param hetmax: float, maximum alternate allele frequency for heterozygous
+        :return: string, H, h, N, or ''
+        -----------------------------------------------------------------------------------------"""
+        genotype = ''
+        depth = self.parsed['depth']
+
+        if depth == 0:
+            # no base present
+            return genotype
+
+        # find the maximum allele
+        depth += 1  # plus 1 prior
+        maxfreq = 0
+        maxallele = ''
+        genotype = 'N'
+        for c in 'ACGTN':
+            freq = (count[c] + count[c.lower()] + 1) / depth
+            if freq > maxfreq:
+                maxfreq = freq
+                maxallele = c
+                minorfreq = 1 - freq
+                if minorfreq < hommax:
+                    genotype = 'H'
+                elif minorfreq > hetmin and minorfreq < hetmax:
+                    genotype = 'h'
+
+        return genotype
+
+    # end of mendel
 
 
 # End of Mpileup ===================================================================================
@@ -130,26 +176,39 @@ class Mpileup:
 
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+    depth_min = 10
+    bias_max = 3
+
     mp = Mpileup()
     mp.open_file('../data/PrFi_10k.mpileup', 'r')
 
     total = {'indel': []}
+
     while mp.parse():
-        # print(mp.countchar())
-        mp.countchar()
-        print('=>', mp.parsed['position'], mp.parsed['depth'])
+        count = mp.countchar()
+        # print('\t', count)
+        genotype = mp.mendel()
 
-        for key in mp.count:
-            if key is 'indel':
-                if mp.count['indel']:
-                    total['indel'] += mp.count['indel']
+        status = ''
+        if mp.parsed['depth'] < depth_min:
+            status += 'L'
+        if abs(count['strand_bias']) >= bias_max:
+            status += 'B'
 
-            elif key in total:
-                total[key] += mp.count[key]
-
-            else:
-                total[key] = mp.count[key]
-
-    print(total)
+        print('{}\t{}\t{}:{}:{}:{}:{}\t{}\t{:.3f}\t{:.3f}\t{}\t{:.3f}\t{}'.format(
+            mp.parsed['position'],
+            mp.parsed['depth'],
+            count['A'] + count['a'],
+            count['C'] + count['c'],
+            count['G'] + count['g'],
+            count['T'] + count['t'],
+            count['N'] + count['n'],
+            genotype,
+            count['strand_bias'],
+            count['end_frac'],
+            count['indel'],
+            count['indel_frac'],
+            status
+        ))
 
     exit(0)
