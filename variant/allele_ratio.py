cc912b026a40b7686e05a755347bbd938ad70e74
Michael Gribskov <mgribsko@gmail.com>
2021-02-05 13:29:54 -0500

    allele_ratio, calculates positional depth, maf, and maf histogram

---
+++ variant/allele_ratio.py
@@ -0,0 +1,46 @@
+"""=================================================================================================
+calculate depth and minor allele fraction along a sequence from mpileup file
+
+Michael Gribskov     05 February 2021
+================================================================================================="""
+import math
+from mpileup import Mpileup
+
+# --------------------------------------------------------------------------------------------------
+# main program
+# --------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    bins = 100
+
+    mp = Mpileup()
+    mp.open_file('../data/PrFi_10k.mpileup', 'r')
+
+    position = []
+    depthpos = []
+    mafpos = []
+    maf_histogram = [0 for _ in range(100)]
+
+    freq = {}
+    while mp.parse():
+        mp.countchar()
+        depth = mp.parsed['depth']
+        depthplus = depth + 1.0
+        for c in 'ACGTN*':
+            freq[c] = (mp.count[c] + 0.167) / depthplus
+
+        freq_order = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
+        major = freq_order[0]
+
+        if not depth or mp.count[major] == depth:
+            continue
+
+        minorfreq = 1.0 - freq[major]
+        mafpos.append(minorfreq)
+        mafh = math.floor(bins * minorfreq)
+        maf_histogram[mafh] += 1
+
+        position.append(mp.parsed['position'])
+        depthpos.append(depth)
+        print('{}\t{}\t{}\t{}\t{:0.3f}'.format(position[-1], major, depthpos[-1], mafh, mafpos[-1]))
+
+    exit(0)
