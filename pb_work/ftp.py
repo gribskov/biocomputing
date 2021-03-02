9ceea21c5fdf81bd85f89169e8a19ad5bd4f82f7
Michael Gribskov <mgribsko@gmail.com>
2021-03-02 13:21:41 -0500

    ftp.py - updated to new ftplib and changes at NCBI

--- pb_work/ftp.py
+++ pb_work/ftp.py
@@ -1,27 +1,30 @@
 from ftplib import FTP
 
+# https://ftp.ncbi.nlm.nih.gov/genomes/genbank/plant/Citrus_sinensis/latest_assembly_versions/GCA_000695605.1_Citrus_sinensis_v1.0/
 ncbi_ftp = 'ftp.ncbi.nlm.nih.gov'
-orange = 'genomes/Citrus_sinensis'
-fasta = 'csi_ref_Csi_valencia_1.0_chr1.fa.gz'
+genome_plant = 'genomes/genbank/plant'
+orange = 'Citrus_sinensis/latest_assembly_versions/GCA_000695605.1_Citrus_sinensis_v1.0'
 
 ftp = FTP(ncbi_ftp, user='anonymous', passwd='gribskov@purdue.edu')
-ftp.cwd(orange)  # change working directory
+cisen = '{}/{}'.format(genome_plant, orange)
+ftp.cwd(cisen)  # change working directory
 
-for file in ftp.nlst():
-    if file.startswith('CHR'):
-        print('\nstarting', file)
-        ftp.cwd(file)
-        for datafile in ftp.nlst():
-            print(datafile, end=' ')
-            if datafile.endswith('.fa.gz'):
-                print('--> retrieving {} ... '.format(datafile), end=' ')
-                try:
-                    ftp.retrbinary("RETR " + datafile, open(datafile, 'wb').write)
-                    print('done')
-                except:
-                    print('Error retrieving {}'.format(fasta))
-            else:
-                print('   skip')
-        ftp.cwd('..')
+# directory listing
+filelist = []
+ftp.dir(filelist.append)
+print(*filelist, sep='\n')
+
+print('\nTransferring files')
+for file in sorted(ftp.nlst()):
+    print('\t{}'.format(file), end=' ')
+    if file.startswith('GCA') and file.endswith('.gz'):
+        print('--> retrieving', end=' ')
+        try:
+            ftp.retrbinary("RETR " + file, open(file, 'wb').write)
+            print('--> done')
+        except:
+            print('--> error')
+    else:
+        print('--> skip')
 
 exit(0)
