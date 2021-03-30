7eceac0b643922ac88aaac3165e0c21bc83d2609
Michael Gribskov <mgribsko@gmail.com>
2021-03-30 13:15:51 -0400

    flask_ex - a slightly more complicated answer that reads a name from login.html and displays a page

--- pb_work/examples/flask_ex/simple.py
+++ pb_work/examples/flask_ex/simple.py
@@ -3,12 +3,23 @@ Example Flask application
 
 Michael Gribskov     30 March 2021
 ================================================================================================="""
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/<worldtype>')
 def welcome(worldtype):
-   return '<H1>Welcome to the {} world </H1>'.format(worldtype)
+    return '<H1>Welcome to the world of {}<H1>'.format(worldtype)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+       user = request.form['user']
+       return redirect(url_for('welcome', worldtype=user))
+   else:
+      user = request.args.get('user')
+      url = '/{}'.format(user)
+      return redirect(url)
+
 
 # --------------------------------------------------------------------------------------------------
 #
