d22a10df14a086716b42fa41d30e6ceb49383250
Michael Gribskov <mgribsko@gmail.com>
2021-04-12 05:52:35 -0400

    examples/recursion - added timing code

--- pb_work/examples/recursion.py
+++ pb_work/examples/recursion.py
@@ -10,11 +10,12 @@ def factorial(n):
     recursive factorial function
     factorial(n) = n * factorial(n-1)
     ---------------------------------------------------------------------------------------------"""
-    if n <= 1:
-        return 1;
+    if n > 1:
+        f = n * factorial(n - 1)
+        return f
 
-    f = n * factorial(n - 1)
-    return f
+    return 1
+
 
 def factorialStack(n):
     """----------------------------------------------------------------------------------
@@ -33,7 +34,18 @@ def factorialStack(n):
 
     return f
 
+# --------------------------------------------------------------------------------------------------
+# main
+# --------------------------------------------------------------------------------------------------
+import timeit
 
-for i in range(6):
-    print(i, factorial(i))
-    print(i, factorialStack(i))
+for i in range(50):
+
+    setup = 'from __main__ import factorial, factorialStack'
+
+    recurse = 'factorial({})'.format(i)
+    print('i={}\trecursive: {:.5f}'.format(
+        i, timeit.timeit(stmt=recurse, setup=setup,number=100000)), end='\t')
+
+    stack = 'factorialStack({})'.format(i)
+    print('stack: {:.5f}'.format(timeit.timeit(stmt=stack, setup=setup, number=100000)))
