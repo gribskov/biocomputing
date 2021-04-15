8323cd2f55d0381eea58e9e810e9f16293b2cc6c
Michael Gribskov <mgribsko@gmail.com>
2021-04-15 12:11:34 -0400

    cluster/tree_multi - with find_comma function

--- cluster/tree_multi.py
+++ cluster/tree_multi.py
@@ -13,6 +13,30 @@ class Tree:
         self.distance = None
         self.children = []
 
+    @staticmethod
+    def find_comma(newick):
+        """-----------------------------------------------------------------------------------------
+        Find the central comma in the newick string
+        this version only works for binary trees
+
+        :param newick:
+        :return: int, pos of comma in string, None if not found
+        -----------------------------------------------------------------------------------------"""
+        depth = 0
+        pos = 0
+        for c in newick:
+            if c == '(':
+                depth += 1
+            elif c == ')':
+                depth -= 1
+            elif c == ',':
+                if depth == 0:
+                    return pos
+
+            pos += 1
+
+        return None
+
     def from_newick(self, newick):
         """-----------------------------------------------------------------------------------------
         Construct the tree by recursively splitting the newick string at central commas until all
@@ -21,10 +45,23 @@ class Tree:
         :return: True
         -----------------------------------------------------------------------------------------"""
         # remove outer parentheses
+        newick = newick.strip(' ;')
+        newick = newick[1:-1]
 
         # find central comma
+        comma = Tree.find_comma(newick)
 
         # build child nodes
+        left = newick[:comma]
+        right = newick[comma + 1:]
+
+        child = Tree()
+        child.from_newick(left)
+        self.children.append(child)
+
+        child = Tree()
+        child.from_newick(right)
+        self.children.append(child)
 
         return True
 
@@ -33,4 +70,10 @@ class Tree:
 # Testing
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+
+    newick = [ '(a,b),(c, (d,e)', 'a,b', 'c, (d,e)']
+    for n in newick:
+        comma = Tree.find_comma(n)
+        print('{} => {}\t\t{}'.format(n, n[:comma], n[comma+1:]))
+
     exit(0)
