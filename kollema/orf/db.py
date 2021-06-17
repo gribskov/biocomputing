a20fce795b51f3df6b8eb39cff155ce22c89c8d3
Michael Gribskov <mgribsko@gmail.com>
2021-06-17 08:46:24 -0400

    db.py - interim commit with calculation on initial model and classification

--- kollema/orf/db.py
+++ kollema/orf/db.py
@@ -1,5 +1,5 @@
 import sys
-import json
+from math import log2
 import pprint
 import pymongo
 from pymongo import MongoClient
@@ -97,6 +97,7 @@ class DB:
             )
 
     def longest_orf_by_gene(self):
+        print('finding longest ORFs for each gene')
         return self.db.transcripts.aggregate([
             # {
             # # limit for testing
@@ -187,11 +188,17 @@ class DB:
             ],
             allowDiskUse=True)
 
-    def length_distribution(self, minlen=0, show=True):
-        """
-        return the orfs greater than minlen and optionally print distribution of length
-        :return:
-        """
+    def get_long_orfs(self, minlen=0, show=True):
+
+        """-----------------------------------------------------------------------------------------
+        return the orfs greater than minlen
+        if show is true, print the length distribution
+
+        :param minlen: int, minimum length for ORF
+        :param show: boolean, print the distribution
+        :return: collection.cursor
+        -----------------------------------------------------------------------------------------"""
+        # select the longest ORF from each gene equal or longer than minlen
         result = self.db.longorfbygene.find({'mxlen': {'$gte': minlen}})
         if show:
             nval = result.count()
@@ -211,14 +218,180 @@ class DB:
 
         return result
 
+    def model_from_counts(self, wordsize=6, initval=1):
+        """-----------------------------------------------------------------------------------------
+        use the counts in the long ORFs to set up word frequency model
+        :param wordsize: int, model word length
+        :param initval: int, prior for counts
+        :return: Word object with counts
+        -----------------------------------------------------------------------------------------"""
+        long = self.get_long_orfs(1200, show=False)
+        model = Word(size=6)
+        model.init_words(initval=1)
+        nword = model.remove_stops_inframe()
+        print(f'{nword} words remaining after removing stops')
+        for rf in long:
+            model.sequence = rf['mxrf']['seq']
+            model.counts_get()
+
+        return model
+
+    def get_rf_frequencies(self, wordsize=6, background=None, block=10000):
+        """-----------------------------------------------------------------------------------------
+        use the counts in the long ORFs to set up word frequency model
+        :param wordsize: int, model word length
+        :param background: Word object, prior for counts, remove unwanted words before using
+        :return: Word object with total counts
+        -----------------------------------------------------------------------------------------"""
+        long = self.get_long_orfs(0, show=False)
+
+        # cloning the background incorporates the prior
+
+        total = background.clone()
+        datablock = []
+        nseq = 0
+        for rf in long:
+            rfcount = background.clone()
+            nseq += 1
+            rfcount.sequence = rf['mxrf']['seq']
+            total = rfcount.counts_get()
+            rfcount.to_frequency()
+
+            datablock.append({'_id': rf['_id'], 'count': rfcount.count})
+            if not nseq % block:
+                self.db.frequency.insert_many(datablock)
+                datablock = []
+                print('.', end='')
+
+        self.db.frequency.insert_many(datablock)
+
+        return total
+
+    def logodds_distribution(self, logodds, wordsize=6, initval=0):
+        """-----------------------------------------------------------------------------------------
+
+        :param logodds:
+        :param wordsize:
+        :param initval:
+        :return:
+        -----------------------------------------------------------------------------------------"""
+
+        # template for counts from each rf
+        template = Word(size=6)
+        template.init_words(initval=0)
+        nword = template.remove_stops_inframe()
+
+        # calculate logodds score for each rf
+        lo_out = open("lo_out.tsv", 'w')
+        nrf = 0
+        for rf in pep.db.longorfbygene.find({}):
+            lo = 0
+            rfcount = template.clone()
+            rfcount.sequence = rf['mxrf']['seq']
+            seqlen = len(rfcount.sequence)
+            invsl = rfcount.size / seqlen
+            rfcount.count_total = 0
+            for word in template.count:
+                rfcount.count[word] = invsl
+                rfcount.count_total += invsl
+
+            rfcount.counts_get()
+            for word in rfcount.count:
+                lo += logodds[word] * rfcount.count[word]
+            # print(f'{rf["_id"]}\t{lo:.1f}\t{rf["mxrf"]["len"]}')
+            lo_out.write(f'{rf["_id"]}\t{lo:.1f}\t{rf["mxrf"]["len"]}\t{lo / rf["mxrf"]["len"]}\n')
+            nrf += 1
+            if not (nrf % 10000):
+                print(nrf)
+
+        lo_out.close()
+
+        return None
+
+
+def checkstop(seq):
+    nstop = 0
+    for pos in range(0, len(seq) - 2, 3):
+        if seq[pos:pos + 3] in ('TAA', 'TAG', 'TGA'):
+            nstop += 1
+    return nstop
+
+
+def composition_from_sequence(dbobj, alphabet='ACGT'):
+    """-----------------------------------------------------------------------------------------
+    Calculate letter frequencies for all sequences in longest orf by gene
+    :param dbobj: Db object
+    :param alphabet: string, allowed keys for count
+    :return: dict, keys are sequence characters
+    -----------------------------------------------------------------------------------------"""
+    comp = {i: 0 for i in alphabet}
+    comp['total'] = 0
+    long = dbobj.get_long_orfs(0, show=False)
+    for rf in long:
+        for letter in rf['mxrf']['seq']:
+            if letter in alphabet:
+                comp[letter] += 1
+                comp['total'] += 1
+
+    return comp
+
+
+def classify_minmax(data, model, frac):
+    """---------------------------------------------------------------------------------------------
+    score reading frame frequencies vs a single model and classify highest score fraction, frac, as
+    model 2, the lowest scoring frac as model 1, and everything else as model 0
+
+    :param data: DB object
+    :param model: Word object (log frequency form)
+    :param frac: float, fraction of top and bottom sequences to classify
+    :return: dict, key in rf ID, value is class
+    ---------------------------------------------------------------------------------------------"""
+    print('assigning to classes')
+    assign = {}
+    score = []
+    block = 10000
+    n = 0
+    for lfreq in data.db.frequency.find({}):
+        s = 0
+        for word in lfreq['count']:
+            s += lfreq['count'][word] * model.count[word]
+        score.append((lfreq['_id'], s))
+
+        n += 1
+        if not n % block:
+            print(f'+{n}')
+
+    score.sort(key=lambda v: v[1])
+    min = int(frac * len(score))
+    max = len(score) - min - 1
+
+    # type = 1
+    # assign = {k[0]: 1 for k in range(0, min)}  # the first min as class 1
+    # assign = {k[0]: 0 for k in range(min, max)}
+    # assign = {k[0]: 2 for k in range(max, len(score))}
+
+    assign = {x[0]: 1 for x in score[:min]}
+    assign.update({x[0]: 0 for x in score[min:max]})
+    assign.update({x[0]: 2 for x in score[max:]})
+
+    return assign
+
 
 # --------------------------------------------------------------------------------------------------
 #
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
+    # setup template for word counts/frequencies
+    # hexamer words minus words with stop codons (which are never present in ORFs)
+    count_template = Word(size=6)
+    count_template.init_words()
+    count_template.remove_stops_inframe()
+
+    # read in transcripts and identify ORFS
     load_data = False
     find_long_orfs = False
+    save_frequencies = False
 
     pep = DB()
     print(pep.db.list_collection_names())
@@ -235,60 +408,42 @@ if __name__ == '__main__':
     if find_long_orfs:
         print('finding longest orfs in each gene')
         # transcripts.create_index('gene')
-        result = pep.longest_orf_by_gene()
+        rfs = pep.longest_orf_by_gene()
         # result.sort(key=lambda r: r['mxrf']['len'])
 
-    def checkstop(seq):
-        nstop = 0
-        for pos in range(0,len(seq)-2,3):
-            if seq[pos:pos+3] in ('TAA', 'TAG', 'TGA'):
-                nstop += 1
-        return nstop
+    # background distribution from random model, used for prior
+    random = count_template.clone()
+    composition = composition_from_sequence(pep)
+    random.count_random(composition)
+    random.to_frequency()
 
-    result = pep.length_distribution(1200, show=False)
-    coding = Word(size=6)
-    coding.init_words(initval=1)
-    nword = coding.remove_stops_inframe()
-    print(f'{nword} words remaining after removing stops')
-    for rf in result:
-        check = checkstop(rf["_id"])
-        if check > 0:
-            print(f'{rf["_id"]}\t{check}')
-            print(f'{rf["_id"]}\t{rf["mxlen"]}\t{len(rf["mxrf"]["seq"])}')
-        coding.sequence = rf['mxrf']['seq']
-        coding.counts_get()
+    if save_frequencies:
+        # for each long orf, save the hexamer frequencies
+        print('calculating word frequencies')
+        pep.db.frequency.drop()
+        total_freq = pep.get_rf_frequencies(wordsize=6, background=random)
 
-    for word in sorted(coding.count, key=lambda k:coding.count[k], reverse=True):
-        # if coding.count[word] == 1:
-        #     break
+    # set up initial models
+    model = [
+        {'name': 'unassigned', 'count': count_template.clone()},
+        {'name': 'coding', 'count': count_template.clone()},
+        {'name': 'noncoding', 'count': count_template.clone()}
+        ]
 
-        print(f'{word}\t{coding.count[word]}')
-        if word.startswith('TAA') or word.endswith('TAA') or \
-            word.startswith('TAG') or word.endswith('TAG') or \
-            word.startswith('TGA') or word.endswith('TGA'):
-            print("oops")
+    # coding model - assume longest ORFs are coding
+    model[1]['count'] = pep.model_from_counts(wordsize=6, initval=1)
+    model[1]['count'].to_logfrequency()
+
+    assign = classify_minmax(pep, model[1]['count'], 0.05)
+    stat = [{'n': 0, 'lenmin': 0, 'lenmax': 0, 'lenave': 0, 'count': count_template.clone()} \
+            for _ in range(3)]
+    for seq in assign:
 
 
-    # s = 0
-    # for i in range(5):
-    #     print(f'i={i}')
-    #     s += i
-
-    # db = MongoClient().test
-    # db.a.delete_many({})
-    # db.a.insert_many([{"a":"dn1"}, {"a":"dn2"}])
+    # logodds = {}
+    # for word in coding.count:
+    #     logodds[word] = log2(coding_freq[word] / noncoding_freq[word])
     #
-    # for i in range(3):
-    #     db.a.update_many({"a":"dn1"}, {"$push":{"d":i}})
-    # db.a.update_many({"a":"dn2"}, {"$set":{"d":[i for i in range(4)]}})
-    # # db.a.update_many({}, {'$push': {'d': [5,6,7]}})
-    # # db.a.update_many({}, {'$set': {'e': {'in':7,'out':10}}})
-    # # db.a.update_many({}, {'$push': {'f': {'in': 7, 'out': 10}}})
-    # result = db.a.find({})
-    # pp = pprint.PrettyPrinter(indent=2, width=60, compact=False)
-    # print = pp.pprint
-    # for doc in result:
-    #     # pp.pprint(doc)
-    #     print(doc)
+    # pep.logodds_distribution(logodds)
 
     exit(0)
