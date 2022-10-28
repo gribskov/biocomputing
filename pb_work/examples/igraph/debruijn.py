df65d96dd8acfda40fc7721ec3100a85a000cb84
Michael Gribskov <mgribsko@gmail.com>
2022-10-28 08:56:35 -0400

    igraph/debruijn updated

--- pb_work/examples/igraph/debruijn.py
+++ pb_work/examples/igraph/debruijn.py
@@ -3,12 +3,10 @@
 
 Michael Gribskov     17 April 2022
 ================================================================================================="""
-from oligo.kmer import Kmer
+from kmer import Kmer
 from oligo.debruijn import KmerSet
 # import matplotlib.pyplot as plt
 import igraph
-
-
 
 
 def debruijn_condense(kmerset):
@@ -31,15 +29,14 @@ def debruijn_condense(kmerset):
             if v2 not in set:
                 continue
             if len(set[v2]['before']) == 1:
-                    
-                new = set[v1]['label'] + set[v2]['label'][k-1:]
-                set[v1]['after'] =  set[v2]['after']
+
+                new = set[v1]['label'] + set[v2]['label'][k - 1:]
+                set[v1]['after'] = set[v2]['after']
                 set[v1]['label'] = new
-                
+
                 del set[v2]
                 if v2 in ids:
                     ids.remove(v2)
-
 
     return kmerset
 
@@ -161,14 +158,77 @@ Tell me no lies, make me a happy man
 Ah ah, ah ah, ah ah, ah ah, ah ah, ah ah, ahhh.
 Ah, yeah!'''
 
+    both_sides_now = '''Rows and flows of angel hair
+And ice cream castles in the air
+And feather canyons everywhere
+Looked at clouds that way
+But now they only block the sun
+They rain and they snow on everyone
+So many things I would have done
+But clouds got in my way
+I've looked at clouds from both sides now
+From up and down and still somehow
+It's cloud illusions I recall
+I really don't know clouds at all'''
 
+    champagne_problems = """You booked the night train for a reason
+    So you could sit there in this hurt
+    Bustling crowds or silent sleepers
+    You're not sure which is worse
+    Because I dropped your hand while dancing
+    Left you out there standing
+    Crestfallen on the landing
+    Champagne problems
+    Your mom's ring in your pocket
+    My picture in your wallet
+    Your heart was glass, I dropped it
+    Champagne problems
+    You told your family for a reason
+    You couldn't keep it in
+    Your sister splashed out on the bottle
+    Now no one's celebrating
+    Dom Perignon, you brought it
+    No crowd of friends applauded
+    Your hometown skeptics called it
+    Champagne problems
+    You had a speech, you're speechless
+    Love slipped beyond your reaches
+    And I couldn't give a reason
+    Champagne problems"""
+    # Your Midas touch on the Chevy door
+    # November flush and your flannel cure
+    # "This dorm was once a madhouse"
+    # I made a joke, "Well, it's made for me"
+    # How evergreen, our group of friends
+    # Don't think we'll say that word again
+    # And soon they'll have the nerve to deck the halls
+    # That we once walked through
+    # One for the money, two for the show
+    # I never was ready so I watch you go
+    # Sometimes you just don't know the answer
+    # 'Til someone's on their knees and asks you
+    # "She would've made such a lovely bride
+    # What a shame she's fucked in the head," they said
+    # But you'll find the real thing instead
+    # She'll patch up your tapestry that I shred
+    # ’tis the damn season
+    # And hold your hand while dancing
+    # Never leave you standing
+    # Crestfallen on the landing
+    # With champagne problems
+    # Your mom's ring in your pocket
+    # Her picture in your wallet
+    # You won't remember all my
+    # Champagne problems
+    # You won't remember all my
+    # Champagne problems"""
 
-    kmer = KmerSet(text=black_dog)
+    kmer = KmerSet(text=champagne_problems)
     print("original text\n", kmer.text)
     textlen = kmer.clean_text()
     print("\ncleaned text\n", kmer.text)
     print("\n{} letters".format(textlen))
-    kmer.from_text(8)
+    kmer.from_text(6)
 
     kmer.link()
     kset = kmer.set
@@ -218,7 +278,16 @@ Ah, yeah!'''
     # h = g.layout_circle()
     # h = g.layout_davidson_harel(maxiter=100, fineiter=50, weight_node_dist=1, weight_border=0, weight_edge_lengths=0.2,
     #                             weight_edge_crossings=2, weight_node_edge_dist=2, cool_fact=0.99)
-    igraph.plot(g, layout=h, bbox=(2000,2000), margin=400)
-
+    style = {'layout':     h,
+             'bbox':       (2500, 2500),
+             'margin':     600,
+             'vertex_label_dist': 5,
+             'vertex_label_size': 20,
+             'edge_arrow_size': 1,
+             'edge_arrow_width':0.7,
+             'edge_width':2,
+             'edge_curved': True
+             }
+    igraph.plot(g, **style)
 
     exit(0)
