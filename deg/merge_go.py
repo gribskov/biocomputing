20f1c406391a6b1e41e8259c1085a83b83cf4568
Michael Gribskov <mgribsko@gmail.com>
2022-11-19 07:48:09 -0500

    merge_go.py first working version

--- deg/merge_go.py
+++ deg/merge_go.py
@@ -14,7 +14,7 @@ class GOset():
 
     ============================================================================================="""
 
-    def __init__(self):
+    def __init__(self, gofile=''):
         """-----------------------------------------------------------------------------------------
 
         -----------------------------------------------------------------------------------------"""
@@ -23,6 +23,12 @@ class GOset():
         self.fh = None
         self.nlines = 0
         self.term = {}
+        self.tag = {'GO': 'GO:'}
+        self.column = []
+
+        if gofile:
+            self.source = gofile
+            self.openfile(gofile, 'r')
 
     def openfile(self, filename, mode='', description=''):
         """-----------------------------------------------------------------------------------------
@@ -44,7 +50,7 @@ class GOset():
 
         return fh
 
-    def read_info(self, column={}, tag={}, tagsplit='|'):
+    def read_info(self, line, tagsplit='|'):
 
         """---------------------------------------------------------------------------------------------
         MSU     Soltu.DM.02G022630.1    Soltu.DM.02G022630.1            GO:0000785      TAIR:AT1G20696  IEA             C                       gene    taxon:4113      20201112        MSU
@@ -54,23 +60,24 @@ class GOset():
         Soltu.DM.01G045700.1    0e68191f38931e266e404adc8d317738        552     TIGRFAM TIGR01035       hemA: glutamyl-tRNA reductase   101     509     4.0E-114        T       11-11-2020      IPR000343     Tetrapyrrole biosynthesis, glutamyl-tRNA reductase       GO:0008883|GO:0033014|GO:0050661|GO:0055114
 
         :return:
-        ---------------------------------------------------------------------------------------------"""
-        line = self.fh.readline()
+        -----------------------------------------------------------------------------------------"""
         info = {}
+        column = self.column
 
         if line:
             self.nlines += 1
-            field = line.rstrip.split()
+            field = line.rstrip().split()
             for col in column:
                 info[col] = field[column[col]]
 
-            for tag in tags:
+            for tag in self.tag:
                 # check for all tags
+                target = self.tag[tag]
                 tagset = []
 
                 for f in field:
                     # check all columns for tagged data
-                    if f.find(tag) > -1:
+                    if f.find(target) > -1:
                         if f.find(tagsplit):
                             tagset += f.split(tagsplit)
                         else:
@@ -78,11 +85,76 @@ class GOset():
 
                 info[tag] = tagset
 
-        self.addinfo(info)
+        self.add_info(info)
 
+        return
+
+    def add_info(self, info):
+        """-----------------------------------------------------------------------------------------
+        merge information about the current GO term into the existing information
+
+        :param info: dict       source_id, tair, GO:
+        :return: int            number of GO terms for this source_id
+        -----------------------------------------------------------------------------------------"""
+        try:
+            id = info['source_id']
+        except KeyError:
+            return 0
+
+        if not id in self.term:
+            # unknown source term, create columns
+            self.term[id] = {x: None for x in self.column}
+            for t in self.tag:
+                self.term[id][t] = []
+
+        entry = self.term[id]
+        for col in info:
+            if col == 'GO':
+                entry[col] += info['GO']
+            else:
+                entry[col] = info[col]
+
+        return len(entry['GO'])
+
+    def load(self, comment='#'):
+        """-----------------------------------------------------------------------------------------
+        load entire data file
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        self.nlines = 0
+        for line in self.fh:
+            if line.startswith(comment):
+                continue
+
+            self.read_info(line)
+
+        return self.nlines
+
+    def golist(self):
+        """-----------------------------------------------------------------------------------------
+        Return a unique list of GO terms in stored for all source IDs
+
+        :return: list   unique GO terms
+        -----------------------------------------------------------------------------------------"""
+        go = set()
+        for source in self.term:
+            go.update(self.term[source]['GO'])
+
+        return sorted(go)
 
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+    gofile = sys.argv[1]
+    go = GOset(gofile)
+    go.column = {'source_id': 1, 'tair': 4}
+    tags = ['GO:']
+    nlines = go.load()
+    sys.stderr.write(f'processed {nlines} lines from {gofile}\n')
+    sys.stderr.write(f'{len(go.term)} sequence IDs\n')
+
+    golist = go.golist()
+    sys.stderr.write(f'{len(golist)} unique GO terms\n')
+
     exit(0)
