098695910205f1d395e8705d35f30de3a17e44c6
Michael Gribskov <mgribsko@gmail.com>
2024-03-04 08:28:30 -0500

    phobius.py - updated to https, added more comments and output

--- pb_work/examples/phobius.py
+++ pb_work/examples/phobius.py
@@ -10,9 +10,8 @@ from bs4 import BeautifulSoup
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-
-    phobius = 'http://phobius.sbc.su.se/'
-    submit = '/cgi-bin/predict.pl'
+    phobius = 'https://phobius.sbc.su.se/'
+    submit = 'cgi-bin/predict.pl'
 
     bri1 = '''>AEE87069.1 Leucine-rich receptor-like protein kinase family protein [Arabidopsis thaliana]
 MKTFSSFFLSVTTLFFFSFFSLSFQASPSQSLYREIHQLISFKDVLPDKNLLPDWSSNKNPCTFDGVTCR
@@ -34,17 +33,19 @@ GYVPPEYYQSFRCSTKGDVYSYGVVLLELLTGKRPTDSPDFGDNNLVGWVKQHAKLRISDVFDPELMKED
 PALEIELLQHLKVAVACLDDRAWRRPTMVQVMAMFKEIQAGSGIDSQSTIRSIEDGGFSTIEMVDMSIKE
 VPEGKL'''
 
-    command = {'protseq':bri1, 'format':'plp'}
-    response = requests.post(phobius+submit, command)
+    command = {'protseq': bri1, 'format': 'plp'}
+    print(f'Sending query to {phobius + submit}')
+    response = requests.post(phobius + submit, data=command)
 
+    # find the image tag with beautiful soup, the image shows the prediction probabilities
     soup = BeautifulSoup(response.content, 'html.parser')
     img = soup.find('img')
-    # print(img['src'])
 
-    # plot address is in image tag, beginning at character 3
-    plot = requests.get(phobius+img['src'][3:],stream=True)
-    print( plot)
-    with open('../pb_work/examples/phobius.png', 'wb') as image:
+    # plot address is in the image tag source, beginning at character 3
+    plot = requests.get(phobius + img['src'][3:], stream=True)
+    print(f"status:{plot} prediction plot: {img['src']}")
+    with open('phobius.png', 'wb') as image:
+        # write out plot in binary format
         image.write(plot.content)
 
     exit(0)
