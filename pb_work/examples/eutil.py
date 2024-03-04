e79d7b6529ff4b5e2d2044f7575512a9b56bc4a0
Michael Gribskov <mgribsko@gmail.com>
2024-03-04 07:33:58 -0500

    updated with API key and clearer output

--- pb_work/examples/eutil.py
+++ pb_work/examples/eutil.py
@@ -1,25 +1,28 @@
 """=================================================================================================
 eutil.py
-retrieve information from NCBI  suing eutil REST interface
-works 2 March 2021
+retrieve information from NCBI  using eutil REST interface
+works 4 March 2024
 ================================================================================================="""
 import requests
 
 # https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=asthma&usehistory=y
-
+api_key = '645c588aed8ff727e6ed8059d10e7db2ea09'
 ncbi = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 esearch = 'esearch.fcgi?'
 
-query = 'db=protein&term=asthma&usehistory=y'
+# Get request
+query = f'api_key={api_key}&db=protein&term=asthma&usehistory=y'
 response = requests.get(ncbi + esearch + query)
-print(response.content)
 
-print('\n', response.text)
+print(f'response content:\n{response.content}')
+print(f'\nresponse text: {response.text}')
 
-params = {'db': 'protein',
+# Post request
+params = {'api_key':'645c588aed8ff727e6ed8059d10e7db2ea09',
+          'db': 'protein',
           'term': 'asthma',
           'usehistory': 'y'}
 
 esearch = 'esearch.fcgi'
 response = requests.post(ncbi + esearch, params)
-print('\n', response.text)
+print(f'\n post response text: {response.text}')
