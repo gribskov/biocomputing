64ad7d2574265ee10a691a57ebdaa8b374564c8f
Michael Gribskov <mgribsko@gmail.com>
2025-01-10 14:09:17 -0500

    Converting toucher to scandir, so it will be faster, also reorganizing to process one directory at a time.

--- utils/toucher.py
+++ utils/toucher.py
@@ -8,9 +8,68 @@ toucher.py <path_to_dir1> [path_to_dir2> ...]
 
 8 January 2019     Michael Gribskov
 ================================================================================================="""
-import sys
+import argparse
+import textwrap as _textwrap
 import os
+import sys
 import time
+from collections import defaultdict
+
+
+# class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter):
+class CustomFormatter(argparse.MetavarTypeHelpFormatter):
+    """=============================================================================================
+    Custom formatter for command line argument help
+    ============================================================================================="""
+
+    def _split_lines(self, text, width=60):
+        """-----------------------------------------------------------------------------------------
+        Gracefully split lines in command line help. lines are split at 60 characters by default
+
+        :param text: str, text to split
+        :param width: int, width to split at
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        text = self._whitespace_matcher.sub(' ', text).strip()
+        self._max_help_position = 100
+        return _textwrap.wrap(text, width)
+
+
+# end of class CustomFormatter
+
+
+def arguments_get():
+    """---------------------------------------------------------------------------------------------
+    Set up command line arguments, read from command line, and store in argparse.ArgumentParser
+    object, cl. Requires customFormatter()
+
+    :return: argparse.ArgumentParser object
+    ---------------------------------------------------------------------------------------------"""
+
+    cl = argparse.ArgumentParser(description='Update access date for old files',
+                                 formatter_class=CustomFormatter,
+                                 allow_abbrev=True)
+    cl.add_argument('-t', '--target', type=str, default="./",
+                    help='target directories (default: %(default)s)')
+    cl.add_argument('-y', '--youngest_date', type=int, default=3,
+                    help='Minimum age in days (default: %(default)s)')
+    cl.add_argument('-o', '--oldest_date', type=int, default=21,
+                    help='Maximum age in days (default: %(default)s)')
+    cl.add_argument('-n', '--noupdate', action='store_true',
+                    help='Report only, do not update (default: %(default)s)')
+
+    return cl.parse_args()  # parse_args  reads the command line
+
+
+def target_split(comma_str):
+    """---------------------------------------------------------------------------------------------
+    The target string may be a comma separated list, split on commas and return as a list
+
+    :param comma_str: str   one or more target directories
+    :return: list           with each directory as a target
+    ---------------------------------------------------------------------------------------------"""
+    target_list = comma_str.split(',')
+    return target_list
 
 
 def find_target_directories(root, purgesec, old):
@@ -53,70 +112,104 @@ def find_target_directories(root, purgesec, old):
 # ==================================================================================================
 roots = []
 
-if len(sys.argv) > 1:
-    roots = sys.argv[1:]
+args = arguments_get()
+target = target_split(args.target)
+ysec = args.youngest_date * 24 * 3600
+osec = args.oldest_date * 24 * 3600
 
-else:
-    # if no starting point is provided use the current working directory
-    roots.append('.')
+print(f'purge target: {args.target}')
+print(f'purge range: {osec} s - {ysec} s')
 
-purgeday = 46  # purge limit in days, set by RCAC, 60 days is the limit so use 60 days - 2 weeks
-purgesec = purgeday * 24 * 3600  # purge limit in seconds
-targetsec = int(
-    0.5 * purgeday * 24 * 3600)  # target after access time adjustment, 50% of purge limit
-print('purge limit: {} day {} sec'.format(purgeday, purgesec))
-print('purge target: {} s'.format(targetsec))
+# use target as a stack, visited records directories that have been seen so they don't get processed
+# twice
+visited = defaultdict(int)
 
-old = {}
-for root in roots:
-    old = find_target_directories(root, purgesec, old)
+# print('{} directories containing stale files found'.format(len(old)))
 
-print('{} directories containing stale files found'.format(len(old)))
+while target:
+    current = target.pop()
+    visited[current] += 1
+    files = os.scandir(current)
+    now = int(time.time())
+    cutoff = now - osec
+    oldest = now
+    updatable = {}
 
-for path in old:
+    for f in files:
 
-    now = time.time()
-    (oldest, newest) = old[path]
-    try:
-        scale = targetsec / (newest - oldest)
-    except ZeroDivisionError:
-        # print('\nerror: directory: {}\tnewest={}\toldest={}'.format(
-        #     path, newest, oldest))
-        scale = 1
 
-    check = now - scale * (newest - oldest)
-    print('\ndirectory: {}\t\t scale={:.3f}\nnewest={}\noldest={}->{}'.format(
-        path, scale, time.ctime(newest), time.ctime(oldest), time.ctime(check)))
+        if f.is_dir():
+            visited[f] += 1
+            target.append(f)
 
-    for file in os.listdir(path):
+        # elif f.is_file:
+        #     print(f'{current}\t{f.name}')
 
-        try:
-            # failures such as broken symbolic links
-            full_file = os.path.join(path, file)
-            stat = os.stat(full_file)
-        except FileNotFoundError:
+        else:
+            # the other possibility is a symbolic link, do nothing
             continue
 
-        if os.path.isdir(full_file):
-            # skip directories
-            continue
+        info = os.stat(f)
+        atime = int(info.st_atime)
+        oldest = min(atime, oldest)
+        if atime < cutoff:
+            updatable[f] = atime
 
-        newaccess = now - scale * (newest - stat.st_ctime)
+    scale = cutoff / oldest
+    print(f'\ndirectory:{current}\tscale:{scale}\tcutoff:{cutoff}\ttoldest:{oldest}')
+    for f in updatable:
+        new = int(updatable[f] * scale)
+        # if new > now:
+        #     continue
+        delta = (updatable[f] - new) / 3600 / 24
+        print(f'{f}\told:{updatable[f]}\tnew:{new}\tdays:{delta}')
 
-        # print(stat)
-        print('accessed: {} -> {}\t{}\t{}\t\t{}\t{}'.format(
-            time.ctime(stat.st_atime),
-            time.ctime(newaccess),
-            file,
-            stat.st_size,
-            time.ctime(stat.st_mtime),
-            time.ctime(stat.st_ctime)))
 
-        # change access time, leave modification time (mtime) and creation time (ctime) the same
-        try:
-            os.utime(full_file, (newaccess, stat.st_mtime))
-        except OSError:
-            sys.stderr.write('OSError:\n')
+
+# for path in old:
+#
+#     now = time.time()
+#     (oldest, newest) = old[path]
+#     try:
+#         scale = targetsec / (newest - oldest)
+#     except ZeroDivisionError:
+#         # print('\nerror: directory: {}\tnewest={}\toldest={}'.format(
+#         #     path, newest, oldest))
+#         scale = 1
+#
+#     check = now - scale * (newest - oldest)
+#     print('\ndirectory: {}\t\t scale={:.3f}\nnewest={}\noldest={}->{}'.format(
+#         path, scale, time.ctime(newest), time.ctime(oldest), time.ctime(check)))
+#
+#     for file in os.listdir(path):
+#
+#         try:
+#             # failures such as broken symbolic links
+#             full_file = os.path.join(path, file)
+#             stat = os.stat(full_file)
+#         except FileNotFoundError:
+#             continue
+#
+#         if os.path.isdir(full_file):
+#             # skip directories
+#             continue
+#
+#         newaccess = now - scale * (newest - stat.st_ctime)
+#
+#         # print(stat)
+#         print('accessed: {} -> {}\t{}\t{}\t\t{}\t{}'.format(
+#             time.ctime(stat.st_atime),
+#             time.ctime(newaccess),
+#             file,
+#             stat.st_size,
+#             time.ctime(stat.st_mtime),
+#             time.ctime(stat.st_ctime)))
+#
+#         # change access time, leave modification time (mtime) and creation time (ctime) the same
+#         try:
+#             os.utime(full_file, (newaccess, stat.st_mtime))
+#         except OSError:
+#             sys.stderr.write('OSError:\n')
 
     # end of loop over files in directory
 # end of loop over directories
