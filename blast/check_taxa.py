0b5765432ca3c2cf667566a57af2e95cfad67080
gribskov <mgribsko@purdue.edu>
2025-02-26 05:24:37 -0500

    check_taxa.py

---
+++ blast/check_taxa.py
@@ -0,0 +1,11 @@
+"""#################################################################################################
+After hand editing, check taxon lists to make sure all have the scientific name (Tax) as well as
+NCBI numeric name (TaxID)
+
+26 February 2025        Michael Gribskov
+#################################################################################################"""
+
+if __name__ == '__main__':
+
+
+    exit(0)
