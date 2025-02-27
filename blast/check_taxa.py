62453b563134273e6bf1fa535d0b28300cbba5d5
Michael Gribskov <mgribsko@gmail.com>
2025-02-27 13:14:17 -0500

    check_taxa.py reading corrected good file works

--- blast/check_taxa.py
+++ blast/check_taxa.py
@@ -4,35 +4,35 @@ NCBI numeric name (TaxID)
 
 26 February 2025        Michael Gribskov
 #################################################################################################"""
+from collections import defaultdict
 
-def read_tsv(fname):
+def read_tsv(fname, ):
     """---------------------------------------------------------------------------------------------
-    read the hand edited tsv file of taxa and taxid
+    read the hand edited tsv file of taxa and taxid, tab separated
 
     field 0: count of matches in the blast search
-    field 1: taxonomic name - continues to first all numerical field
+    field 1: taxonomic name - continues to first all numerical field (may have mutliple terms)
+    field 2: taxonomic id - the first all digit field after the
+
 
     :param fname: string    path to tsv data file
     :return: list of dict   data read from file
     ---------------------------------------------------------------------------------------------"""
     f = open(fname, 'r')
-    out = []
+    out = defaultdict(lambda: {'species':set(), 'taxid':set()})
     for line in f:
         print(f'{line}')
         field = line.rstrip().split('\t')
         tax = field[1]
+        this = out[tax]
+
         for i in range(2,len(field)):
             if field[i].isdigit():
                 break
+            this['species'].add(field[i])
 
-            tax += f' {field[i]}'
+        this['taxid'].add(int(field[i]))
 
-        taxid = ' '.join(field[i:])
-        if not taxid.isdigit():
-            print(f'|{tax}|{taxid}|')
-
-
-        out.append(line)
 
     return out
 
