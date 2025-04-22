eb3edb87d6190ebed9b3068e44eb77d67f7f50bc
Michael Gribskov <mgribsko@gmail.com>
2025-04-22 14:07:38 -0400

    blast_to_taxonomy.py in progress adding output of sequences grouped by trinity and separated taxonomically

--- blast/blast_to_taxonomy.py
+++ blast/blast_to_taxonomy.py
@@ -136,7 +136,7 @@ def send_query_block_jgi(qlist, taxa):
             taxstr = 'Error;Taxon_not_found'
 
         try:
-            taxstr = taxstr.replace(' ','_')
+            taxstr = taxstr.replace(' ', '_')
             if not taxstr:
                 taxstr = 'None'
             taxa[query]['lineage'] = taxstr
@@ -151,7 +151,7 @@ def send_query_block_jgi(qlist, taxa):
         except Exception as err:
             # any other errors
             errstr = f'{err}'
-            taxa[query]['lineage'] = f'Error; {errstr.replace(' ','_')}'
+            taxa[query]['lineage'] = f'Error; {errstr.replace(' ', '_')}'
 
     return ok_n
 
@@ -286,31 +286,36 @@ def blast(blastfile):
 
 def blast_get_tax(stitle):
     """---------------------------------------------------------------------------------------------
-    get the Tax string from the subject title field
+    get the Tax string and numerical ID from the subject title field
 
     :param stitle: string       subject title from blast search
-    :return: string             Tax string
+    :return: string, int        Tax string, taxid
     ---------------------------------------------------------------------------------------------"""
+    # taxid is in a qualifier 'TaxID=xxx '
+    taxidpos = stitle.find('TaxID') - 1
+    taxid_end = stitle.find('RepID') - 1
+    taxid = int(stitle[taxidpos + 7:taxid_end])
+
     taxpos = stitle.find('Tax=') + 4
-    taxidpos = stitle.find('TaxID')
-    return stitle[taxpos:taxidpos]
+    return stitle[taxpos:taxidpos], taxid
 
 
 def blast_read(blastfile):
     """---------------------------------------------------------------------------------------------
     Read the taxonmic string from the blast search and make a list of unique taxa. Spaces in the
-    taxon name are conveted to underlines, after filtering parenthesized phrases
+    taxon name are converted to underlines, after filtering parenthesized phrases
 
     :param blastfile: string    path to file with blast result
     :return: dict               taxonomic string is the key, lineage is empty to be filled later
     ---------------------------------------------------------------------------------------------"""
     nline = 0
-    taxa = defaultdict(lambda: {'lineage': '', 'group': 'other', 'count': 0})
+    taxa = defaultdict(lambda: {'taxid': 0, 'lineage': '', 'group': 'other', 'count': 0})
 
     for hit in blast(blastfile):
         nline += 1
-        tax = blast_get_tax(hit['stitle'])
+        tax, taxid = blast_get_tax(hit['stitle'])
 
+        # trim some unecessary/confusing parts from the taxonomy string
         if tax.find('(') != -1:
             # trim off information in parentheses
             # this information is usually not part of the actual taxonomic name
@@ -320,6 +325,7 @@ def blast_read(blastfile):
         # convert spaces to underline
         tax = tax.strip().replace(' ', '_')
         tax = tax.replace('/', '_')
+
         # remove strain information (sometimes causes multiple returns from taxonomy server)
         strain = tax.find('_str.')
         if strain > -1:
@@ -336,7 +342,12 @@ def blast_read(blastfile):
         # if tax.find('str.')>-1:
         #     print(tax)
         taxa[tax]['count'] += 1
+        taxa[tax]['taxid'] = taxid
         print(f'{nline:6d}\t{tax}')
+
+        # debug
+        if nline > 10000:
+            break
 
     print(f'\nblast results processed: {nline}')
     print(f'unique taxa {len(taxa)}')
@@ -344,9 +355,27 @@ def blast_read(blastfile):
     return taxa
 
 
+def blast_trinity_cluster(blastfile):
+    """---------------------------------------------------------------------------------------------
+    gather results for trinity ids clustered at a specified level. each truncated trinity id is a
+    list of blast results (dicts) with the taxid extracted and added to the dicts
+
+    :param blastfile: string        path to blast result file
+    :return: dict                   key is truncated trinity ID, value is list of hits
+    ---------------------------------------------------------------------------------------------"""
+    cluster = defaultdict(lambda: [])
+    for hit in blast(blastfile):
+        tid = trinity_filter_id(hit['qid'], 3, True)
+        taxstr, taxid = blast_get_tax(hit['stitle'])
+        hit['taxid'] = taxid
+        cluster[tid].append(hit)
+
+    return cluster
+
+
 def assign_groups(taxa, group):
     """---------------------------------------------------------------------------------------------
-    use the search terms in group to assign assemblies to defined groups. sequence not matching
+    use the search terms in group to assign assemblies to defined groups. sequences not matching
     are defined as other. error sequences are defined as 'error'
 
     :param taxa: dict       content: {'lineage': , 'group': , 'count': }
@@ -373,6 +402,62 @@ def assign_groups(taxa, group):
     return count
 
 
+def trinity_filter_id(tid, level=3, remove_trinity=True):
+    """---------------------------------------------------------------------------------------------
+    filter a trinity id to truncate it at the desired level, and optionally drop the redundant
+    TRINITY_ prefix
+
+    level 4: all parts, TRINITY_DN221212_c0_g1_i1
+    level 3: gene,      TRINITY_DN221212_c0_g1
+    level 2: component, TRINITY_DN221212_c0
+    level 1: bundle,    TRINITY_DN221212
+
+    :param tid: string                  trinity sequence ID
+    :param level: int                   level to trim trinity IDs, default is gene level
+    :param remove_trinity: boolean      remove the use TRINITY_ prefix if true
+    :return: string                     trinity ID trimmed at the desired level
+    ---------------------------------------------------------------------------------------------"""
+    first = 0
+    last = level + 1
+    if remove_trinity:
+        first = 1
+
+    field = tid.split('_')
+    return '_'.join(field[first:last])
+
+
+def group_assign(merged):
+    """---------------------------------------------------------------------------------------------
+    assign the merged isoforms to a group based on the counts of the groups of each constituent
+    isoform. The rules are:
+    1) assign as plant if there are any plants
+    2) assign as highest count
+    3) if it's a tie, assign as other
+
+    :param merged: list of Cluster      annotated merged clusters
+    :return: string                     count of number assigned to each group
+    ---------------------------------------------------------------------------------------------"""
+    count = defaultdict(int)
+    group = 'other'
+    for iso in merged:
+        if iso['group'] == 'plant':
+            count['plant'] += 1
+            continue
+
+        # gsort = sorted(this.ngroup, key=lambda g: this.ngroup[g], reverse=True)
+        # if len(gsort) == 1:
+        #     gbest = gsort[0]
+        # elif gsort[0] > gsort[1]:
+        #     gbest = gsort[0]
+        # else:
+        #     gbest = 'other'
+        #
+        # this.group = gbest
+        # count[gbest] += 1
+
+    return count
+
+
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
@@ -381,9 +466,9 @@ if __name__ == '__main__':
     goodfile = sys.argv[2]
 
     # define groups - each groups is defined by one taxonomic term, anything undefined is 'other'
-    group = {'error': 'Error', 'plant': 'Viridiplantae', 'virus': 'Viruses',
-             'arthropod': 'Arthropoda', 'fungi': 'Fungi', 'bacteria': 'Bacteria' }
-    gorder = {'error': 1, 'plant': 3, 'virus': 4, 'arthropod': 5, 'fungi': 6, 'bacteria': 7, 'other': 2 }
+    group = {'error':     'Error', 'plant': 'Viridiplantae', 'virus': 'Viruses',
+             'arthropod': 'Arthropoda', 'fungi': 'Fungi', 'bacteria': 'Bacteria'}
+    gorder = {'error': 1, 'plant': 3, 'virus': 4, 'arthropod': 5, 'fungi': 6, 'bacteria': 7, 'other': 2}
 
     good_n = 0
     bad_n = 0
@@ -410,39 +495,53 @@ if __name__ == '__main__':
 
     # assign to groups based on lineage
     groupcount = assign_groups(taxa, group)
+    taxa_invert = {}
+    for t in taxa:
+        taxa_invert[taxa[t]['taxid']] = taxa[t]
+        taxa_invert[taxa[t]['taxid']]['taxstr'] = t
+
+    for t in sorted(taxa_invert):
+        print(f'{t}\t{taxa_invert[t]}')
 
     # ----------------------------------------------------------------------------------------------
     # results
     # ----------------------------------------------------------------------------------------------
-    print(f'\nCounts per group found in {blastfile}')
-    for g in groupcount:
-        print(f'\t{g}\t{groupcount[g]}')
+    # print(f'\nCounts per group found in {blastfile}')
+    # for g in groupcount:
+    #     print(f'\t{g}\t{groupcount[g]}')
+    #
+    # good = open(goodfile, 'w')
+    #
+    # # species with top counts in blast result
+    # ntop = 100
+    # good.write(f'\n! top {ntop} taxa:\n')
+    # print(f'\ntop {ntop} taxa:')
+    # n = 0
+    #
+    # for t in sorted(taxa, key=lambda c: taxa[c]['count'], reverse=True):
+    #     good.write(f'!{n:3d}{t:>40s}{taxa[t]['group']:>10s}{taxa[t]['count']:>10d}  {taxa[t]['lineage']}\n')
+    #     print(f'{n:3d}{t:>40s}{taxa[t]['group']:>10s}{taxa[t]['count']:10d}  {taxa[t]['lineage']}')
+    #     n += 1
+    #     if n == ntop:
+    #         break
+    #
+    # good.write(f'\n! Taxa by group\n')
+    # group_old = ''
+    # n = 0
+    # for tax in sorted(taxa, key=lambda t: (gorder[taxa[t]['group']],taxa[t]['lineage'])):
+    #     n += 1
+    #     this = taxa[tax]
+    #     good.write(f"{n:5d}{this['group']:>12s} {tax:>68s}\t{this['lineage']}\n")
+    #
+    #
+    # print(f'\nresults written to {goodfile}')
+    # good.close()
 
-    good = open(goodfile, 'w')
+    # reprocess the blast results, pooling at trinity level and summarizing taxonomic information
+    trinity_cluster = blast_trinity_cluster(blastfile)
+    for tc in trinity_cluster:
+        for iso in trinity_cluster[tc]:
+            g = taxa_invert[iso['taxid']]['group']
 
-    # species with top counts in blast result
-    ntop = 100
-    good.write(f'\n! top {ntop} taxa:\n')
-    print(f'\ntop {ntop} taxa:')
-    n = 0
-
-    for t in sorted(taxa, key=lambda c: taxa[c]['count'], reverse=True):
-        good.write(f'!{n:3d}{t:>40s}{taxa[t]['group']:>10s}{taxa[t]['count']:>10d}  {taxa[t]['lineage']}\n')
-        print(f'{n:3d}{t:>40s}{taxa[t]['group']:>10s}{taxa[t]['count']:10d}  {taxa[t]['lineage']}')
-        n += 1
-        if n == ntop:
-            break
-
-    good.write(f'\n! Taxa by group\n')
-    group_old = ''
-    n = 0
-    for tax in sorted(taxa, key=lambda t: (gorder[taxa[t]['group']],taxa[t]['lineage'])):
-        n += 1
-        this = taxa[tax]
-        good.write(f"{n:5d}{this['group']:>12s} {tax:>68s}\t{this['lineage']}\n")
-
-
-    print(f'\nresults written to {goodfile}')
-    good.close()
 
     exit(0)
