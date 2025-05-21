0f43f6fa86707579f28cb166141baeea93f29620
Michael Gribskov <mgribsko@gmail.com>
2025-05-21 09:24:31 -0400

    prime_streaming.py works but time is min 66.8 ms. slow because every not prime is visited

--- pb_work/kata/prime_streaming.py
+++ pb_work/kata/prime_streaming.py
@@ -4,7 +4,7 @@
 Michael Gribskov     21 May 2025
 ================================================================================================="""
 import sys
-import time
+from time import perf_counter as time
 from collections import defaultdict
 
 class Primes:
@@ -13,22 +13,26 @@ class Primes:
     def stream():
         stack = defaultdict(lambda: [])
         yield 2
-        i = 3
+        yield 3
+        stack[6] = [3]
+        i = 5
         while True:
-            if i in stack:
-                # not prime, remove from stack and replace with incremented
-
-                for val in stack[i]:
-                    stack[val + i].append(val)
-                # del stack[i]
-                stack.pop(i)
-
-            else:
-                # prime, add multiple value and increment to stack
+            next_composite = min(stack)
+            while i < next_composite:
+                # add primes up to the next composite
                 yield i
-                stack[i * i].append(i * 2)
+                # stack[i * i].append(i * 2)
+                stack[i * 2].append(i)
+                i += 2
 
-            i += 2
+
+            # update stack with composite
+            for val in stack[next_composite]:
+                stack[val + next_composite].append(val)
+            del stack[next_composite]
+            if i == next_composite:
+                i += 2
+
             # sys.stderr.write(f'{i}  min={min(stack)}\n')
 
         return
@@ -42,7 +46,8 @@ def verify(from_n, *vals):
     for _ in range(from_n): next(stream)
     ok = True
     for v in vals:
-        if v != next(stream):
+        ns = next(stream)
+        if v != ns:
             ok = False
             break
 
@@ -53,12 +58,12 @@ elapse_min = 10000
 elapse_max = 0
 repeat = 20
 for i in range(repeat):
-    start = time.time()
+    start = time()
     verify(0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
     verify(10, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
     verify(100, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601)
     verify(1000, 7927, 7933, 7937, 7949, 7951, 7963, 7993, 8009, 8011, 8017)
-    end = time.time()
+    end = time()
     delta = end - start
     elapse += end - start
     elapse_min = min(elapse_min, delta)
