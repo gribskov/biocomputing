19272fc5ad32562d85044ebf9082f535426e681a
Michael Gribskov <mgribsko@gmail.com>
2025-09-05 13:50:15 -0400

    plot_transcripts this version works, but occasionally gets the labels wrong du to the sorting by begin position (i think)

--- gff/plot_transcripts.py
+++ gff/plot_transcripts.py
@@ -4,6 +4,7 @@ plot transcripts from GTF file
 Michael Gribskov     04 September 2025
 ================================================================================================="""
 from gff import Gff
+import matplotlib.pylab as plt
 
 
 def split_attributes_gtf(anno):
@@ -27,13 +28,63 @@ def split_attributes_gtf(anno):
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     gtf = 'data/merged.gtf'
+    begin = 1
+    end = 80000
+    target_feature = ['transcript', 'exon']
+    sequence = ['LG01']
+
     anno = Gff(file=gtf)
     anno.del_attr = True
-    anno.get_by_sequence_range(['transcript', 'exon'], ['LG01'], 1, 200000)
+    anno.get_by_sequence_range(target_feature, sequence, begin, end)
     for feature in anno.data:
         print(feature)
 
-    for feature in sorted(anno.data, key=lambda x: x['begin']):
-        print(f'{feature['sequence']}\t{feature['feature']}\t{feature['begin']}\t{feature['end']}')
+    fig, ax = plt.subplots()
 
+    y = -2
+    current = ''
+    width = []
+    left = []
+    ypos = []
+    label = []
+    color = []
+    # for feature in sorted(anno.data, key=lambda x: x['begin']):
+    for feature in anno.data:
+        # if feature['feature'] == 'transcript':
+        #     continue
+        if feature['transcript_id'] != current:
+            if current:
+                # ax.barh(ypos, width=width, left=left, tick_label=label)
+                ax.barh(ypos, width=width, left=left, color=color)
+            current = feature['transcript_id']
+            width = []
+            left = []
+            ypos = []
+            color = []
+            # label = []
+            y += 2
+
+        print(f'{feature['sequence']}\t{feature['feature']}\t{feature['transcript_id']}\t{feature['begin']}',end='')
+        print(f'\t{feature['end']}\t{feature['strand']}')
+        width.append(feature['end'] - feature['begin'] + 1)
+        left.append(feature['begin'])
+
+        if feature['feature'] == 'transcript':
+            ypos.append(y+1)
+            label.append('')
+            color.append('0.75')
+            label.append(feature['transcript_id'])
+        else:
+            ypos.append(y)
+            color.append('r')
+
+    # ax.barh(ypos, width=width, left=left, tick_label=label)
+    ax.barh(ypos, width=width, left=left, color=color)
+    plt.yticks(range(len(label)), labels=label)
+
+    ax.set(xlabel='Position', ylabel='Transcript', title='Exon and Transcript position')
+    ax.grid()
+
+    # fig.savefig("test.png")
+    plt.show()
     exit(0)
