7104e26f34f7104a5cb7fb8844793fa48fba9bc7
Michael Gribskov <mgribsko@gmail.com>
2025-10-08 08:12:34 -0400

    select added indexing function to Sequence to index on any named attribute

--- sequence/bioinfo_sequences/select.py
+++ sequence/bioinfo_sequences/select.py
@@ -52,7 +52,28 @@ class Sequence():
 
         return len(self.item)
 
+    def index(self, key, nfield):
+        """-----------------------------------------------------------------------------------------
+        make an index use the attribute given as key. Use only the first nfield fields
 
+        :param key: string      self.item[]['attr']['key']
+        :param nfield: int      number of fields to use from the attribute, key
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        idx = {}
+        n_item = 0
+        for item in self.item:
+            value = item['attr'][key]
+            field = value.split()
+            idxterm = field[nfield-1]
+            if idxterm in idx:
+                idx[idxterm].append(n_item)
+            else:
+                idx[idxterm] = [n_item]
+
+            n_item += 1
+
+        return idx
 
 
 # --------------------------------------------------------------------------------------------------
@@ -65,8 +86,22 @@ if __name__ == '__main__':
     pro = Sequence(inpro)
     nuc = Sequence(innuc)
 
-    genus = nuc.index('organism_name')
+    genusn = nuc.index('organism_name', 1)
+    genusp = pro.index('organism_name', 1)
 
-    for p in pro.item:
+    # sourcep = pro.index('pub_gene_id', 1)
+
+    n_g = 0
+    for g in genusp:
+        n_g += 1
+
+        pstr = '\t'
+        if g in genusp:
+            pstr = f'\t{genusp[g]}'
+        nstr = '\t'
+        if g in genusn:
+            nstr = f'\t{genusn[g]}'
+
+        print(f'{n_g}\t{g}\tp:{pstr}\tn:{nstr}')
 
     exit(0)
