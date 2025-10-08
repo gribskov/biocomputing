237be3a42e41dec88bfb472bab6bbcab458cc849
Michael Gribskov <mgribsko@gmail.com>
2025-10-08 15:38:21 -0400

    select.py se;ecting sequences working need to write out

--- sequence/bioinfo_sequences/select.py
+++ sequence/bioinfo_sequences/select.py
@@ -75,11 +75,24 @@ class Sequence():
 
         return idx
 
+    def fasta(self, outfh, linelen=100):
+        """-----------------------------------------------------------------------------------------
+        write sequence to outfh in Fasta format
+
+        :param outfh:
+        :param linelen: int
+        :return:
+        -----------------------------------------------------------------------------------------"""
+
 
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
+    lenfrac = 0.1
+    lower = 1.0 - lenfrac
+    upper = 1.0 + lenfrac
+
     inpro = open(sys.argv[1], 'r')
     innuc = open(sys.argv[2], 'r')
 
@@ -89,19 +102,75 @@ if __name__ == '__main__':
     genusn = nuc.index('organism_name', 1)
     genusp = pro.index('organism_name', 1)
 
+    # average length
+    n_pro = 0
+    sum_len = 0
+    for p in pro.item:
+        sum_len += len(p['sequence'])
+        n_pro += 1
+
+    ave_len = sum_len / n_pro
+    min_len = ave_len * lower
+    max_len = ave_len * upper
+    min_nuc = min_len * 3
+    max_nuc = max_len * 3
+    print(f'Average protein length: {ave_len:.1f}')
+
+
     # sourcep = pro.index('pub_gene_id', 1)
 
     n_g = 0
     for g in genusp:
         n_g += 1
 
-        pstr = '\t'
-        if g in genusp:
-            pstr = f'\t{genusp[g]}'
-        nstr = '\t'
-        if g in genusn:
-            nstr = f'\t{genusn[g]}'
+        lenok = False
+        for pid in genusp[g]:
+            plen = len(pro.item[pid]['sequence'])
+            if plen < min_len:
+                # too short
+                continue
+            elif plen > max_len:
+                # too long
+                continue
+            else:
+                # length ok
+                lenok = True
 
-        print(f'{n_g}\t{g}\tp:{pstr}\tn:{nstr}')
+            if not lenok:
+                # no suitable protein found, try next protein for this genus
+                print(f'not suitable: {pid}\t{pro.item[pid]['attr']['organism_name']}\tlen:{len(pro.item[pid]['sequence'])}')
+                continue
+
+            # check the corresponding nucleotide sequence
+            if pid < len(nuc.item):
+                nlen = len(nuc.item[pid]['sequence'])
+            else:
+                # no matching nucleotide
+                print(f'skipped/no nucleotide: {pid}\t{pro.item[pid]['attr']['organism_name']}\tlen:{len(pro.item[pid]['sequence'])}')
+                break
+
+            if nlen > min_nuc and nlen < max_nuc:
+                # nucleotide length is ok
+                # write out pro and nuc
+                # skip other protein entries
+                print(f'match {pid}\t{g}\t{pro.item[pid]['attr']['organism_name']}\tlen:',end=' ')
+                print(f'{len(pro.item[pid]['sequence'])},{len(nuc.item[pid]['sequence'])}')
+                break
+            else:
+                # nucleotide is not ok, try another protein
+                continue
+
+        # end of loop over proteins in genus
+
+
+
+        # pstr = '\t'
+        # if g in genusp:
+        #     pstr = f'\t{genusp[g]}'
+        # nstr = '\t'
+        # if g in genusn:
+        #     nstr = f'\t{genusn[g]}'
+        #
+        # print(f'{n_g}\t{g}\tp:{pstr}\tn:{nstr}')
 
     exit(0)
