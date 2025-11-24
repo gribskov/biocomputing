1ba3f776de1931dc3270de6760ddc9a55a11f597
gribskov <mgribsko@purdue.edu>
2025-11-24 15:12:53 -0500

    merge_stringtie_gtf.py first working version

--- gff/merge_stringtie_gtf.py
+++ gff/merge_stringtie_gtf.py
@@ -1,12 +1,70 @@
 """=================================================================================================
 Merge overlapping transcripts in GTF file to produce transcripts suitable for use with Salmon
-(or Kallisto). Works on a single input file such as the one procuced by stringtie merge
+(or Kallisto). Works on a single input file such as the one produced by stringtie merge
 
 1. Only records with the desired feature(s), usually gene or transcript
 2. merged features begin at the earliest beginning and end at the latest end point
 ================================================================================================="""
-from gff import Gff
+from gff2 import GxfSet
 from ranges.linear_range import Range
+
+
+class Lrange:
+    """---------------------------------------------------------------------------------------------
+
+    ---------------------------------------------------------------------------------------------"""
+
+    def __init__(self):
+        self.name = ''
+        self.seqid = ''
+        self.start = None
+        self.end = None
+        self.strand = None
+        self.members = []
+
+
+def overlap(flist, space=1000):
+    """---------------------------------------------------------------------------------------------
+    find overlap between features
+    
+    :param features: GxfSet     collection of GxfRecord features
+    :return: list               merged features, collection of Lrange
+    ---------------------------------------------------------------------------------------------"""
+    merged = []
+    current = Lrange()
+
+    range_n = 0
+    feature_n = 0
+    for f in sorted(flist.features, key=lambda x: (x.seqid, x.strand, x.start)):
+        # if f.seqid != 'LG01':
+        #     continue
+
+        feature_n += 1
+        if (f.seqid != current.seqid or
+                f.strand != current.strand or
+                f.start - current.end > space):
+
+            # start new range
+            if current.name:
+                merged.append(current)
+            current = Lrange()
+            current.name = f'range_{range_n}'
+            range_n += 1
+            current.seqid = f.seqid
+            current.start = f.start
+            current.end = f.end
+            current.strand = f.strand
+            current.members.append(f)
+            continue
+
+        # merge
+        current.end = max(current.end, f.end)
+        current.members.append(f)
+
+        print(f)
+
+    return merged
+
 
 # ##################################################################################################
 # main program
@@ -20,19 +78,19 @@ if __name__ == '__main__':
     print(f'\tSelected features: {features}')
 
     # read in gtf with selected features
-    gtf = Gff(file=gtffile)
-    merged = Range()
-    feature_n = 0
-    for f in gtf.get_by_feature('transcript'):
-        feature_n += 1
-        merged.features.append(f)
-
-
-
-    # print(f'\t{features} features read: {len(gtf.data)}')
+    gtf = GxfSet(file=gtffile)
+    feature_n = gtf.feature_get(features)
+    print(f'{feature_n} features read from {gtffile}')
 
     # overlap
+    merged = overlap(gtf)
+    print(f'merged: {len(merged)}')
 
     # created merged names and write out as gtf
+
+    output = 'merged.out'
+    mout = open(output, 'w')
+    for f in merged:
+        mout.write(f'{f.seqid}\tStringTie\ttranscript\t{f.start}\t{f.end}\t.\t{f.strand}\t.\n')
 
     exit(0)
