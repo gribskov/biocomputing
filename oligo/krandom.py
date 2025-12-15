29fc072194f8b6797711067c7159afa203008e38
Michael Gribskov <mgribsko@gmail.com>
2025-12-15 15:08:55 -0500

    krandom.py initial version, working on arborescence code (randomtreewithroot)

---
+++ oligo/krandom.py
@@ -0,0 +1,91 @@
+"""=================================================================================================
+Exact kmer preserving randomization
+
+Michael Gribskov     15 December 2025
+================================================================================================="""
+import random
+from collections import defaultdict
+
+
+class Word:
+    """=============================================================================================
+
+    Synopsis
+        (None yet)
+
+    Michael Gribskov
+    ============================================================================================="""
+    k = 3
+
+    def __init__(self, label=''):
+        """-----------------------------------------------------------------------------------------
+        word constructor
+        -----------------------------------------------------------------------------------------"""
+        self.label = ''
+        self.out = []
+
+        if label:
+            self.label = label
+
+    def randomtreewithroot(self):
+        """-----------------------------------------------------------------------------------------
+        pseudocode in
+        Jiang, M., Anderson, J., Gillespie, J. et al. uShuffle: A useful tool for shuffling
+        biological sequences while preserving the k-let counts. BMC Bioinformatics 9, 192 (2008).
+        https://doi.org/10.1186/1471-2105-9-192
+
+        :return:
+        -----------------------------------------------------------------------------------------"""
+
+        in_tree = []
+        next = []
+        for i in range(0,n):
+            in_tree[i] = False
+
+        next[r] = None
+        in_tree[r] = True
+        for i in range(0,n):
+            u = i
+
+        while not in_tree[u]:
+            next[u] = self.randomsuccessor(u)
+            u = next[u]
+
+  
+        u = i
+        while not in_tree[u]:
+            in_tree[u] = True
+            u = next[u]
+        
+        return next
+
+    def randomsuccessor(self):
+        return
+        
+# ==================================================================================================
+# Testing
+# ==================================================================================================
+if __name__ == '__main__':
+    k = 3
+    alpha = 'ACGT'
+    len = 20
+    sequence = ''
+    for l in range(len):
+        base = random.choice(alpha)
+        sequence += base
+        print(f'{base}:{sequence}')
+
+    # overlap = defaultdict(Word)
+    words = defaultdict(Word)
+    prev = ''
+    for pos in range(0, len - k + 1):
+        kmer = sequence[pos:pos + k]
+        print(kmer)
+
+        words[kmer].label = kmer
+        # overlap[kmer[1:-1]].out.append(words[kmer])
+        if prev:
+            words[prev].out.append(words[kmer])
+            # overlap[prev[1:-1]].out.append(words[kmer])
+
+    exit(0)
