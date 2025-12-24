97227dc84a2efb167061a4838424cf435e759655
gribskov <mgribsko@purdue.edu>
2025-12-24 14:24:29 -0500

    krandom.py appears to work,needs testing

--- oligo/krandom.py
+++ oligo/krandom.py
@@ -60,31 +60,155 @@ def randomtreewithroot(nodelist):
 
 def randomsuccessor(self):
     return
-        
+
+# another try
+class DBG:
+    """
+
+    """
+
+    def __init__(self, sequence='', k=3):
+        """-----------------------------------------------------------------------------------------
+
+        -----------------------------------------------------------------------------------------"""
+        self.k = k
+        self.sequence = sequence
+        self.nodes = set()
+        self.root = ''
+        self.adj = defaultdict(list)
+        self.last_exit = defaultdict(str)
+
+        if sequence:
+            self.build_graph()
+
+
+    def build_graph(self):
+        """-----------------------------------------------------------------------------------------
+        Construct the De Bruijn graph with the specified kmer
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        k = self.k
+        sequence = self.sequence
+        nodes = self.nodes
+        adj = self.adj
+        for i in range(len(sequence) - k + 1):
+            u = sequence[i: i + k - 1]
+            v = sequence[i + 1: i + k]  # Actually next node is sequence[i+1 : i+k]
+            adj[u].append(v)
+            nodes.add(u)
+            nodes.add(v)
+
+        self.root = u
+        return len(nodes)
+
+    def arborescence(self):
+        """-----------------------------------------------------------------------------------------
+
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        nodes = self.nodes
+        last_exit = self.last_exit
+
+        adj = self.adj
+
+        # last_exit = {}
+        in_tree = {self.root}
+
+        for start_node in nodes:
+            if start_node in in_tree: continue
+
+            curr = start_node
+            path = {}
+            while curr not in in_tree:
+                # Pick a random neighbor from the original transition list
+                # Note: For exact k-mer counts, pick from available edges
+                next_node = random.choice(adj[curr])
+                path[curr] = next_node
+                curr = next_node
+
+            # Add the loop-erased path to the tree
+            curr = start_node
+            while curr not in in_tree:
+                last_exit[curr] = path[curr]
+                in_tree.add(curr)
+                curr = path[curr]
+
+        return
+
+    def shuffle(self):
+        """-----------------------------------------------------------------------------------------
+        shuffle all edges EXCEPT the 'last-exit' edges
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        adj = self.adj
+        last_exit = self.last_exit
+
+        for u in adj:
+            edges = adj[u]
+            if u in last_exit:
+                # Move the last_exit edge to the end of the list
+                lastpos = edges.index(last_exit[u])
+                edges[lastpos], edges[-1] = edges[-1],edges[lastpos]
+                random.shuffle(edges[:-1])
+                # edges.remove(last_exit[u])
+                # random.shuffle(edges)
+                # edges.append(last_exit[u])
+            else:
+                random.shuffle(edges)
+
+            adj[u] = edges
+
+        return
+
+    def traverse(self):
+        """-----------------------------------------------------------------------------------------
+
+        :return:
+        -----------------------------------------------------------------------------------------"""
+        adj = self .adj
+        curr = self.sequence[:k-1]
+        result = [curr]
+
+        while adj[curr]:
+            next_node = adj[curr][0]
+            adj[curr].remove(next_node)
+            result.append(next_node[-1])  # Append the last character
+            curr = next_node
+
+        return "".join(result)
 # ==================================================================================================
 # Testing
 # ==================================================================================================
 if __name__ == '__main__':
     k = 3
     alpha = 'ACGT'
-    len = 20
-    sequence = ''
-    for l in range(len):
-        base = random.choice(alpha)
-        sequence += base
-        print(f'{base}:{sequence}')
+    # sequence = ('AAAACCCAAAA')
+    sequence = ('AAAACCCAAAA')
+    g = DBG(sequence, 3)
+    g.arborescence()
+    g.shuffle()
+    shuffled = g.traverse()
+    print(f'{sequence} -> {shuffled}')
+
+    exit(0)
+
+
+    # for l in range(len):
+    #     base = random.choice(alpha)
+    #     sequence += base
+    #     print(f'{base}:{sequence}')
 
     # overlap = defaultdict(Word)
-    words = defaultdict(Word)
-    prev = ''
-    for pos in range(0, len - k + 1):
-        kmer = sequence[pos:pos + k]
-        print(kmer)
-
-        words[kmer].label = kmer
-        # overlap[kmer[1:-1]].out.append(words[kmer])
-        if prev:
-            words[prev].out.append(words[kmer])
-            # overlap[prev[1:-1]].out.append(words[kmer])
+    # words = defaultdict(Word)
+    # prev = ''
+    # for pos in range(0, len - k + 1):
+    #     kmer = sequence[pos:pos + k]
+    #     print(kmer)
+    #
+    #     words[kmer].label = kmer
+    #     # overlap[kmer[1:-1]].out.append(words[kmer])
+    #     if prev:
+    #         words[prev].out.append(words[kmer])
+    #         # overlap[prev[1:-1]].out.append(words[kmer])
 
     exit(0)
